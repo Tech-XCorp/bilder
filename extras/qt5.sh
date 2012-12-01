7aeb1d24ac34b0e9838d1eed42bbd29dac462dc9
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2012-12-01 16:17:25 +0000

    Ruby is needed for qt 5
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@21 a9e16012-fd73-49ee-95cb-036de7809213

--- extras/qt5.sh
+++ extras/qt5.sh
@@ -6,6 +6,5 @@ LINUX
 
 # Make sure the following are installed:
 
-libicu-devel libxcb-devel libXrender-devel xcb-util-wm-devel xcb-util-devel xcb-util-image-devel xcb-util-keysyms-devel
+libicu-devel libxcb-devel libXrender-devel xcb-util-wm-devel xcb-util-devel xcb-util-image-devel xcb-util-keysyms-devel ruby
 
-add -icu to the configureation
