260a49c9ded76ba8094e3030babbde58c62091ca
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2012-12-01 14:26:07 +0000

    Saving some info.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@20 a9e16012-fd73-49ee-95cb-036de7809213

---
+++ extras/qt5.sh
@@ -0,0 +1,11 @@
+BUILDING Qt 5
+
+$Id$
+
+LINUX
+
+# Make sure the following are installed:
+
+libicu-devel libxcb-devel libXrender-devel xcb-util-wm-devel xcb-util-devel xcb-util-image-devel xcb-util-keysyms-devel
+
+add -icu to the configureation
