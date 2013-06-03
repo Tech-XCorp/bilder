f68be3bc3445eb27a8f04fdcc4f8cfa76598112d
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2013-06-03 16:05:40 +0000

    Updating to be able to build the VisIt version of VTK-6
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@174 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/vtk.sh
+++ packages/vtk.sh
@@ -9,16 +9,14 @@
 ######################################################################
 #
 # Version.
-# This is the patched version from the VisIt repo:
-# wget http://portal.nersc.gov/svn/visit//trunk/third_party/vtk-5.0.0i.tar.gz
-# Then repacked:
-# tar xzf vtk-5.0.0i.tar.gz
-# mv VTK vtk-5.0.0i
-# tar czf vtk-5.0.0i.tar.gz vtk-5.0.0i
+# This is directly from the VisIt repo at
+# svn export https://portal.nersc.gov/svn/visit/trunk/third_party/VTK-5e3c539.tar.gz
+# Build info taken from
+# svn export svn export http://portal.nersc.gov/svn/visit/trunk/src/svn_bin/bv_support/bv_vtk.sh
 #
 ######################################################################
 
-VTK_BLDRVERSION=${VTK_BLDRVERSION:-"5.0.0i"}
+VTK_BLDRVERSION=${VTK_BLDRVERSION:-"5e3c539"}
 
 ######################################################################
 #
@@ -26,7 +24,8 @@ VTK_BLDRVERSION=${VTK_BLDRVERSION:-"5.0.0i"}
 #
 ######################################################################
 
-VTK_BUILDS=${VTK_BUILDS:-"ser"}
+VTK_BUILDS=${VTK_BUILDS:-"$FORPYTHON_BUILD"}
+VTK_BUILD=$FORPYTHON_BUILD
 VTK_DEPS=mesa,cmake
 
 ######################################################################
@@ -37,93 +36,127 @@ VTK_DEPS=mesa,cmake
 
 buildVtk() {
 
-  if bilderUnpack vtk; then
-    local VTK_LD_RUN_PATH=
-    local VTK_OS_ARGS=
-    local VTK_PYTHON_ARGS=
+  if ! bilderUnpack VTK; then
+    return
+  fi
+
+# Set up variables
+  local VTK_PREFIX="VTK"
+  local VTK_LD_RUN_PATH=
+  local VTK_OS_ARGS=
+  local VTK_PKG_ARGS=
+  local VTK_PYTHON_ARGS=
 # build_visit sets both of these the same for Darwin
-    local MANGLED_MESA_LIB=libOSMesa${SHOBJEXT}
-    local MANGLED_OSMESA_LIB=libOSMesa${SHOBJEXT}
-    case `uname` in
-      CYGWIN*) # Add /MT flags
-        VTK_OS_ARGS="$VTK_OS_ARGS -DVTK_USE_VIDEO_FOR_WINDOWS:BOOL=OFF"
+  local MANGLED_MESA_LIB=libOSMesa${SHOBJEXT}
+  local MANGLED_OSMESA_LIB=libOSMesa${SHOBJEXT}
+  case `uname` in
+    CYGWIN*) # Add /MT flags
+      VTK_OS_ARGS="$VTK_OS_ARGS -DVTK_USE_VIDEO_FOR_WINDOWS:BOOL=OFF"
 # Cygwin on focus apparently need this
-        VTK_PYTHON_ARGS="-DPYTHON_LIBRARY:FILEPATH=$PYTHON_LIB"
-        case `uname` in
-          CYGWIN*64) # 64-bit Windows needs help finding correct python paths
-            VTK_PYTHON_ARGS="$VTK_PYTHON_ARGS -DPYTHON_EXECUTABLE:FILEPATH='$PYTHON' -DPYTHON_INCLUDE_DIR:PATH=$PYTHON_INCDIR"
-           ;;
-        esac
+      VTK_PYTHON_ARGS="-DPYTHON_LIBRARY:FILEPATH=$PYTHON_LIB"
+      case `uname` in
+        CYGWIN*64) # 64-bit Windows needs help finding correct python paths
+          VTK_PYTHON_ARGS="$VTK_PYTHON_ARGS -DPYTHON_EXECUTABLE:FILEPATH='$PYTHON' -DPYTHON_INCLUDE_DIR:PATH=$PYTHON_INCDIR"
+         ;;
+      esac
 # Protect against jom, as dependencies not right
-        VTK_BUILD_ARGS="-m nmake"
-        ;;
-      Darwin)	# make -j can fail on Darwin
-        VTK_OS_ARGS="$VTK_OS_ARGS -DVTK_USE_CARBON:BOOL=OFF -DVTK_USE_COCOA:BOOL=ON"
-        case `uname -r` in
-          9.*)
-            ;;
-          10.*)
-            VTK_OS_ARGS="$VTK_OS_ARGS -DCMAKE_BUILD_WITH_INSTALL_RPATH:BOOL=ON"
-            ;;
-        esac
-        ;;
-      Linux)
-        VTK_LD_RUN_PATH=${PYTHON_LIBDIR}:${CONTRIB_DIR}/mesa-${MESA_BLDRVERSION}-mgl/lib:$LD_RUN_PATH
-        trimvar VTK_LD_RUN_PATH ':'
-        if test -n "$VTK_LD_RUN_PATH"; then
-          local VTK_LD_RUN_ARGS="LD_RUN_PATH=$VTK_LD_RUN_PATH"
-        fi
-        local VTK_LD_LIB_PATH="$LD_LIBRARY_PATH"
-        if test -n "$LIBFORTRAN_DIR"; then
-         VTK_LD_LIB_PATH="$LIBFORTRAN_DIR:$VTK_LD_LIB_PATH"
-        fi
-        case $PYTHON_LIB_LIBDIR in
-          /usr/lib | /usr/lib64)
-            ;;
-          *)
-            VTK_LD_LIB_PATH="$PYTHON_LIB_LIBDIR:$VTK_LD_LIB_PATH"
-            ;;
-        esac
-        trimvar VTK_LD_LIB_PATH ':'
-        if test -n "$VTK_LD_LIB_PATH"; then
-          local VTK_LD_LIB_ARGS="LD_LIBRARY_PATH=$VTK_LD_LIB_PATH"
-         fi
-        techo "VTK_LD_LIB_ARGS = $VTK_LD_LIB_ARGS."
-        if test -n "$VTK_LD_RUN_ARGS" -o -n "$VTK_LD_LIB_ARGS"; then
-          local VTK_ENV=$VTK_LD_RUN_ARGS,$VTK_LD_LIB_ARGS
-        fi
-        trimvar VTK_ENV ','
-        if test -n "$VTK_ENV"; then
-          VTK_ENV_ARGS="-e $VTK_ENV"
-        fi
-        VTK_MAKE_ARGS="$VTK_MAKEJ_ARGS"
+      VTK_BUILD_ARGS="-m nmake"
+      ;;
+    Darwin)	# make -j can fail on Darwin
+      VTK_OS_ARGS="$VTK_OS_ARGS -DVTK_USE_CARBON:BOOL=OFF -DVTK_USE_COCOA:BOOL=ON"
+      case `uname -r` in
+        9.*)
+          ;;
+        10.*)
+          VTK_OS_ARGS="$VTK_OS_ARGS -DCMAKE_BUILD_WITH_INSTALL_RPATH:BOOL=ON"
+          ;;
+      esac
+      ;;
+    Linux)
+      VTK_LD_RUN_PATH=${PYTHON_LIBDIR}:${CONTRIB_DIR}/mesa-${MESA_BLDRVERSION}-mgl/lib:$LD_RUN_PATH
+      trimvar VTK_LD_RUN_PATH ':'
+      if test -n "$VTK_LD_RUN_PATH"; then
+        local VTK_LD_RUN_ARGS="LD_RUN_PATH=$VTK_LD_RUN_PATH"
+      fi
+      local VTK_LD_LIB_PATH="$LD_LIBRARY_PATH"
+      if test -n "$LIBFORTRAN_DIR"; then
+       VTK_LD_LIB_PATH="$LIBFORTRAN_DIR:$VTK_LD_LIB_PATH"
+      fi
+      case $PYTHON_LIB_LIBDIR in
+        /usr/lib | /usr/lib64)
+          ;;
+        *)
+          VTK_LD_LIB_PATH="$PYTHON_LIB_LIBDIR:$VTK_LD_LIB_PATH"
+          ;;
+      esac
+      trimvar VTK_LD_LIB_PATH ':'
+      if test -n "$VTK_LD_LIB_PATH"; then
+        local VTK_LD_LIB_ARGS="LD_LIBRARY_PATH=$VTK_LD_LIB_PATH"
+       fi
+      techo "VTK_LD_LIB_ARGS = $VTK_LD_LIB_ARGS."
+      if test -n "$VTK_LD_RUN_ARGS" -o -n "$VTK_LD_LIB_ARGS"; then
+        local VTK_ENV=$VTK_LD_RUN_ARGS,$VTK_LD_LIB_ARGS
+      fi
+      trimvar VTK_ENV ','
+      if test -n "$VTK_ENV"; then
+        VTK_ENV_ARGS="-e $VTK_ENV"
+      fi
+      VTK_MAKE_ARGS="$VTK_MAKEJ_ARGS"
 # build_visit uses MANGLED_OSMESA_LIB=libMesaGL on Linux.
-        MANGLED_OSMESA_LIB=libMesaGL${SHOBJEXT}	# per build_visit
-        if test -z "$PYTHON"; then
-          techo "PYTHON NOT SET.  VTK Python wrappers will not build."
-          return
-        fi
-        VTK_PYTHON_ARGS="-DPYTHON_EXECUTABLE:FILEPATH='$PYTHON' -DPYTHON_INCLUDE_DIR:PATH=$PYTHON_INCDIR -DPYTHON_LIBRARY:FILEPATH=$PYTHON_SHLIB"
-        VTK_OS_ARGS="$VTK_OS_ARGS -DCMAKE_BUILD_WITH_INSTALL_RPATH:BOOL=ON"
-        ;;
-    esac
-    local VTK_COMPILERS
-    local VTK_FLAGS
+      MANGLED_OSMESA_LIB=libMesaGL${SHOBJEXT}	# per build_visit
+      if test -z "$PYTHON"; then
+        techo "PYTHON NOT SET.  VTK Python wrappers will not build."
+        return
+      fi
+      VTK_PYTHON_ARGS="-DPYTHON_EXECUTABLE:FILEPATH='$PYTHON' -DPYTHON_INCLUDE_DIR:PATH=$PYTHON_INCDIR -DPYTHON_LIBRARY:FILEPATH=$PYTHON_SHLIB"
+      VTK_OS_ARGS="$VTK_OS_ARGS -DCMAKE_BUILD_WITH_INSTALL_RPATH:BOOL=ON"
+      ;;
+  esac
+  local VTK_COMPILERS
+  local VTK_FLAGS
 
 # For all, build shared.
-    VTK_OS_ARGS="$VTK_OS_ARGS -DBUILD_SHARED_LIBS:BOOL=ON -DVTK_WRAP_PYTHON:BOOL=ON"
+  VTK_OS_ARGS="$VTK_OS_ARGS -DBUILD_SHARED_LIBS:BOOL=ON -DVTK_WRAP_PYTHON:BOOL=ON"
 # CYGWIN uses serial compilers, does not use Mesa
 # Others use gcc, use mesa
-    case `uname` in
-      CYGWIN*)
-        VTK_COMPILERS="-DCMAKE_C_COMPILER:FILEPATH='$CC' -DCMAKE_CXX_COMPILER:FILEPATH='$CXX'"
-        ;;
-      *)
-        VTK_COMPILERS="$CMAKE_COMPILERS_PYC"
-        VTK_FLAGS="$CMAKE_COMPFLAGS_PYC"
-        VTK_MESA_ARGS="-DVTK_USE_MANGLED_MESA:BOOL=ON -DMANGLED_MESA_INCLUDE_DIR:PATH=$CONTRIB_DIR/mesa-mgl/include -DMANGLED_MESA_LIBRARY:FILEPATH=$CONTRIB_DIR/mesa-mgl/lib/${MANGLED_MESA_LIB} -DMANGLED_OSMESA_INCLUDE_DIR:PATH=$CONTRIB_DIR/mesa-mgl/include -DMANGLED_OSMESA_LIBRARY:FILEPATH=$CONTRIB_DIR/mesa-mgl/lib/${MANGLED_OSMESA_LIB}"
-        ;;
-    esac
+  VTK_COMPILERS="$CMAKE_COMPILERS_PYC"
+  VTK_FLAGS="$CMAKE_COMPFLAGS_PYC"
+  case `uname` in
+    CYGWIN*)
+      VTK_COMPILERS="-DCMAKE_C_COMPILER:FILEPATH='$CC' -DCMAKE_CXX_COMPILER:FILEPATH='$CXX'"
+      ;;
+    Linux)
+      VTK_MESA_ARGS="-DVTK_USE_MANGLED_MESA:BOOL=ON -DMANGLED_MESA_INCLUDE_DIR:PATH=$CONTRIB_DIR/mesa-mgl/include -DMANGLED_MESA_LIBRARY:FILEPATH=$CONTRIB_DIR/mesa-mgl/lib/${MANGLED_MESA_LIB} -DMANGLED_OSMESA_INCLUDE_DIR:PATH=$CONTRIB_DIR/mesa-mgl/include -DMANGLED_OSMESA_LIBRARY:FILEPATH=$CONTRIB_DIR/mesa-mgl/lib/${MANGLED_OSMESA_LIB}"
+      ;;
+  esac
+
+# Packages
+# Turn off module groups
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -D${VTK_PREFIX}_Group_Imaging:BOOL=OFF"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -D${VTK_PREFIX}_Group_MPI:BOOL=OFF"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -D${VTK_PREFIX}_Group_Qt:BOOL=OFF"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -D${VTK_PREFIX}_Group_Rendering:BOOL=OFF"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -D${VTK_PREFIX}_Group_StandAlone:BOOL=OFF"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -D${VTK_PREFIX}_Group_Tk:BOOL=OFF"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -D${VTK_PREFIX}_Group_Views:BOOL=OFF"
+
+# Turn on individual modules. dependent modules are turned on automatically
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkCommonCore:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkFiltersFlowPaths:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkFiltersHybrid:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkFiltersModeling:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkGeovisCore:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkIOEnSight:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkIOGeometry:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkIOLegacy:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkIOPLY:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkIOXML:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkInteractionStyle:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkRenderingAnnotation:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkRenderingFreeType:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkRenderingFreeTypeOpenGL:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtkRenderingOpenGL:BOOL=ON"
+  VTK_PKG_ARGS="${VTK_PKG_ARGS} -DModule_vtklibxml2:BOOL=ON"
 
 # Per build_visit:
 # Linking OSMesa for MANGLED_MESA_LIBRARY is correct here; we'll never use
@@ -131,13 +164,12 @@ buildVtk() {
 # we always want to use the 'system's GL library.
 # The only time this is not done is Linux-static
 # For Windows, try not replacing the RELEASE flags
-    local VTK_CONFIG_ARGS="$VTK_COMPILERS $VTK_FLAGS $VTK_OS_ARGS -DBUILD_TESTING:BOOL=OFF -DUSE_ANSI_STD_LIB:BOOL=ON -DVTK_USE_HYBRID:BOOL=ON ${VTK_MESA_ARGS} -DVTK_USE_TK:BOOL=OFF $VTK_PYTHON_ARGS $VTK_SER_OTHER_ARGS"
-    echo VTK_CONFIG_ARGS=$VTK_CONFIG_ARGS
+  local VTK_CONFIG_ARGS="$VTK_COMPILERS $VTK_FLAGS $VTK_OS_ARGS -DBUILD_TESTING:BOOL=OFF -DBUILD_DOCUMENTATION:BOOL=OFF -D${VTK_PREFIX}_ALL_NEW_OBJECT_FACTORY:BOOL=TRUE -DUSE_ANSI_STD_LIB:BOOL=ON -DVTK_USE_HYBRID:BOOL=ON ${VTK_PKG_ARGS} ${VTK_MESA_ARGS} -DVTK_USE_TK:BOOL=OFF $VTK_PYTHON_ARGS $VTK_SER_OTHER_ARGS"
+  echo VTK_CONFIG_ARGS=$VTK_CONFIG_ARGS
 # Pass with commas and separate later.
-    if bilderConfig $VTK_ENV_ARGS vtk ser "$VTK_CONFIG_ARGS"; then
+  if bilderConfig $VTK_ENV_ARGS VTK $VTK_BUILD "$VTK_CONFIG_ARGS"; then
 # Build
-      bilderBuild $VTK_ENV_ARGS $VTK_BUILD_ARGS vtk ser "$VTK_MAKE_ARGS"
-    fi
+    bilderBuild $VTK_ENV_ARGS $VTK_BUILD_ARGS VTK $VTK_BUILD "$VTK_MAKE_ARGS"
   fi
 
 }
@@ -159,12 +191,10 @@ testVtk() {
 ######################################################################
 
 installVtk() {
-  if bilderInstall $VTK_ENV_ARGS $VTK_BUILD_ARGS -r vtk ser; then
+  if bilderInstall $VTK_ENV_ARGS $VTK_BUILD_ARGS -r VTK $VTK_BUILD; then
     case `uname` in
-      CYGWIN*)
-        ;;
-      *)
-        runnrExec "rm -f $CONTRIB_DIR/vtk-$VTK_BLDRVERSION-ser/include/MangleMesaInclude"
+      Linux)
+        runnrExec "rm -f $CONTRIB_DIR/vtk-$VTK_BLDRVERSION-$VTK_BUILD/include/MangleMesaInclude"
         runnrExec "ln -s $CONTRIB_DIR/mesa-$MESA_BLDRVERSION-mgl/include/GL $CONTRIB_DIR/vtk-$VTK_BLDRVERSION-ser/include/MangleMesaInclude"
         ;;
     esac
