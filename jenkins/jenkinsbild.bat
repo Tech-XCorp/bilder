c43a358fc8b5fe957eaf0d8b2fcfec75bb65895b
techxdws <techxdws@a9e16012-fd73-49ee-95cb-036de7809213>
2013-07-04 23:34:09 +0000

    Now checking to see if workspace path is in windows format or not before trying to convert.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@236 a9e16012-fd73-49ee-95cb-036de7809213

--- jenkins/jenkinsbild.bat
+++ jenkins/jenkinsbild.bat
@@ -8,47 +8,67 @@ rem
 rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem rem
 
 ECHO jenkinsbild.bat: ========= EXECUTING jenkinsbild.bat ========
+ECHO jenkinsbild.bat: ========= EXECUTING jenkinsbild.bat ======== > jenkinsbild.log
 ECHO jenkinsbild.bat: starting up in %CD% with arguments, %*.
 ECHO jenkinsbild.bat: starting up in %CD% with arguments, %*. >> jenkinsbild.log
 ECHO jenkinsbild.bat: ++++++JENKINS VARIABLES++++++
+ECHO jenkinsbild.bat: ++++++JENKINS VARIABLES++++++ >> jenkinsbild.log
 ECHO jenkinsbild.bat: JENKINS_HOME=%JENKINS_HOME%
+ECHO jenkinsbild.bat: JENKINS_HOME=%JENKINS_HOME% >> jenkinsbild.log
 ECHO jenkinsbild.bat: JOB_NAME=%JOB_NAME%
+ECHO jenkinsbild.bat: JOB_NAME=%JOB_NAME% >> jenkinsbild.log
 ECHO jenkinsbild.bat: BUILD_ID=%BUILD_ID%
+ECHO jenkinsbild.bat: BUILD_ID=%BUILD_ID% >> jenkinsbild.log
 ECHO jenkinsbild.bat: BUILD_TAG=%BUILD_TAG%
+ECHO jenkinsbild.bat: BUILD_TAG=%BUILD_TAG% >> jenkinsbild.log
 ECHO jenkinsbild.bat: JAVA_HOME=%JAVA_HOME%
+ECHO jenkinsbild.bat: JAVA_HOME=%JAVA_HOME% >> jenkinsbild.log
 ECHO jenkinsbild.bat: WORKSPACE=%WORKSPACE%
+ECHO jenkinsbild.bat: WORKSPACE=%WORKSPACE% >> jenkinsbild.log
 ECHO jenkinsbild.bat: +++++++++++++++++++++++++++++
+ECHO jenkinsbild.bat: +++++++++++++++++++++++++++++ >> jenkinsbild.log
 
 REM JOB_NAME contains <JOB>\<NODES>=<NODE>
 for /f "tokens=1 delims=/" %%A in ("%JOB_NAME%") do set JOB_LINK=%%A
 echo jenkinsbild.bat: JOB Part of JOB_NAME=%JOB_LINK%
+echo jenkinsbild.bat: JOB Part of JOB_NAME=%JOB_LINK% >> jenkinsbild.log
 
 for %%A in ("%CD%") do set drive=%%~dA
 set JOB_LINK=%drive%\%JOB_LINK%
 echo jenkinsbild.bat: Adding drive letter... JOB_LINK=%JOB_LINK%
+echo jenkinsbild.bat: Adding drive letter... JOB_LINK=%JOB_LINK% >> jenkinsbild.log
 REM Jenkins workspace variable has forward slashes, but
 REM we need a windows path, so we convert
 set JENKINS_WSPATH=%WORKSPACE%
 set JENKINS_WSPATH=%JENKINS_WSPATH:/=\%
-set JENKINS_WSPATH=%drive%%JENKINS_WSPATH%
-echo JENKINS_WSPATH=%JENKINS_WSPATH%
 
-if not exist %JOB_LINK% goto createlink else linkexistscontinue
-:createlink
-echo jenkinsbild.bat: Creating soft link with: mklink /D %JOB_LINK% %JENKINS_WSPATH%
-mklink /D %JOB_LINK% %JENKINS_WSPATH%
+REM If workspace path doesn't contain drive letter
+REM then we add the current drive letter to the path.
+set foundcolon=false
+set teststr=%JENKINS_WSPATH%
+if not x%teststr::=%==x%teststr% set foundcolon=true
+if "%foundcolon%"=="false" ( 
+  echo jenkinsbild.bat: Adding drive, %drive%, to WS path.
+  echo jenkinsbild.bat: Adding drive, %drive%, to WS path. >> jenkinsbild.log
+  set JENKINS_WSPATH=%drive%%JENKINS_WSPATH%
+)
+echo jenkinsbild.bat: JENKINS_WSPATH=%JENKINS_WSPATH%
+echo jenkinsbild.bat: JENKINS_WSPATH=%JENKINS_WSPATH% >> jenkinsbild.log
+
+if exist %JOB_LINK% goto linkexistscontinue 
+  echo on
+  mklink /D %JOB_LINK% %JENKINS_WSPATH%
+  echo off
 :linkexistscontinue
 cd %JOB_LINK%
 
 ECHO jenkinsbild.bat: Working in %CD%.
-ECHO jenkinsbild.bat: Working in %CD%. >>jenkinsbild.log
+ECHO jenkinsbild.bat: Working in %CD%. >> jenkinsbild.log
 
 
 C:\CYGWIN\bin\cygpath %CD% >temp.txt
 set /p CYGWINDIR= < temp.txt
 del temp.txt
-rem ECHO CYGWINDIR= %CYGWINDIR%.
-rem ECHO 2= %2
 set JBILDERR=0
 @ECHO on
 C:\CYGWIN\bin\bash --login %CYGWINDIR%/bilder/jenkins/jenkinsbild %*
