17d2ab0c51b010fce6707304b449c1a25b87c2e0
swsides <swsides@a9e16012-fd73-49ee-95cb-036de7809213>
2013-08-01 22:42:27 +0000

    Adding appropriate enviroment variables to the make line to override those in the stock src/Make files.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@331 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/lammps.sh
+++ packages/lammps.sh
@@ -35,10 +35,17 @@ buildLammps() {
   # Specific variables for LAMMPS 'by-hand' make files
   LAMMPS_OTHER_ARGS="LMP_INC=-DLAMMPS_GZIP"
 
+  # Serial flags
+  LAMMPS_SER_COMP_ARGS="CC=$CC LINK=$CC"
   LAMMPS_SER_ARGS="FFT_INC='-DFFT_FFTW -I$CONTRIB_DIR/fftw/include' FFT_PATH='-L$CONTRIB_DIR/fftw/lib' FFT_LIB='-lfftw -lrfftw'"
+  LAMMPS_SER_ARGS="$LAMMPS_SER_COMP_ARGS $LAMMPS_SER_ARGS $LAMMPS_OTHER_ARGS"
   echo "LAMMPS_SER_ARGS = $LAMMPS_SER_ARGS"
 
-  LAMMPS_PAR_ARGS="FFT_INC='-DFFT_FFTW -I$CONTRIB_DIR/fftw-par/include' FFT_PATH='-L$CONTRIB_DIR/fftw-par/lib' FFT_LIB='-lfftw_mpi -lrfftw_mpi'"
+  # Par flags (check mpi version)
+  LAMMPS_PAR_COMP_ARGS="CC=$MPICXX LINK=$MPICC"
+  LAMMPS_PAR_ARGS="FFT_INC='-DFFT_FFTW -I$CONTRIB_DIR/fftw-par/include' FFT_PATH='-L$CONTRIB_DIR/fftw-par/lib' FFT_LIB='-lfftw -lrfftw -lfftw_mpi -lrfftw_mpi'"
+  LAMMPS_PAR_ARGS="$LAMMPS_PAR_ARGS MPI_INC='-I$CONTRIB_DIR/openmpi/include' MPI_PATH='-L$CONTRIB_DIR/openmpi/lib'"
+  LAMMPS_PAR_ARGS="$LAMMPS_PAR_COMP_ARGS $LAMMPS_PAR_ARGS $LAMMPS_OTHER_ARGS"
   echo "LAMMPS_PAR_ARGS = $LAMMPS_PAR_ARGS"
 
   if bilderUnpack lammps; then
@@ -46,8 +53,8 @@ buildLammps() {
     ARGS="$LAMMPS_SER_ARGS mac"
     makeLammps ser "$ARGS"
 
-    # ARGS="$LAMMPS_PAR_ARGS mac_mpi"
-    # makeLammps par "$ARGS"
+    ARGS="$LAMMPS_PAR_ARGS mac_mpi"
+    makeLammps par "$ARGS"
 
   fi
 
@@ -64,7 +71,6 @@ buildLammps() {
 installLammps() {
   putLammps ser
   putLammps par
-
 }
 
 ######################################################################
@@ -79,19 +85,7 @@ installLammps() {
 
 makeLammps() {
 
-  echo "------------------ Must make stubs first -----------"
-
-  # LAMMPS_SRC="GNUmakefile mk src util"
-  # LICENSE README bench doc examples lib potentials python src tools
-  # LAMMPS_SRC="LICENSE README bench doc examples lib potentials python src tools"
-  # LAMMPS_SRC="src/*"
-  # for targ in $LAMMPS_SRC; do
-  #   cmd="cp -R $LAMMPS_BUILD_TOPDIR/$targ $LAMMPS_BUILD_DIR"
-  #   echo "$cmd"
-  #   $cmd
-  # done
-
-  echo "---------- Calling makeLammps with $1 $2 --------"
+  techo -2 "---------- Calling makeLammps with $1 $2 --------"
   BLDTYPE=$1
   ARGS=$2
 
@@ -100,36 +94,35 @@ makeLammps() {
     # 'by-hand configure' by copying all files in src to LAMMPS_BUILD_DIR
     local BLDDIR=LAMMPS_`genbashvar ${BLDTYPE}`_BUILD_DIR
     eval LAMMPS_BUILD_DIR=\$$BLDDIR
-    echo "LAMMPS_BUILD_DIR=$LAMMPS_BUILD_DIR"
+    techo -2 "LAMMPS_BUILD_DIR=$LAMMPS_BUILD_DIR"
     LAMMPS_BUILD_TOPDIR=$LAMMPS_BUILD_DIR/..
-    echo "LAMMPS_BUILD_TOPDIR=$LAMMPS_BUILD_TOPDIR"
-    # !!!!!!!!!!!!!!!!!!! watch this line !!!!!!!!!!!!!!!!!!!
+    techo -2 "LAMMPS_BUILD_TOPDIR=$LAMMPS_BUILD_TOPDIR"
+    techo -2 "------------ Watch this copy line --------------"
     cmd="cp -R $LAMMPS_BUILD_TOPDIR/src/* $LAMMPS_BUILD_DIR"
-    echo "$cmd"
+    techo -2 "$cmd"
     $cmd
 
     # Special make target for serial
-    echo "------ making stubs target by default -------"
-    cd $LAMMPS_BUILD_DIR/STUBS
-    make stubs
-    cd $LAMMPS_BUILD_TOPDIR
+    # Going to STUBS directly because stubs target not working
+    if [ $BLDTYPE == "ser" ]; then
+	techo -2 "-- Making stubs target by default"
+	cd $LAMMPS_BUILD_DIR/STUBS
+	echo "current directory `pwd`"
+	make
+	cd $LAMMPS_BUILD_TOPDIR
+    fi
 
-
-    #
     # Build lammps (because of the bizarre make file structure
     # this build is in-place in the src subdirectory)
-    # 
-    echo "------ This is the current directory" `pwd`
     bilderBuild lammps $BLDTYPE "$JMAKEARGS $ARGS"
 
     # Wait for build, move lib*.a to /lib
-#    waitBuild lammps-$BLDTYPE
-#    if ! test -d $LAMMPS_BUILD_DIR/lib; then
-#      echo "creating directory $LAMMPS_BUILD_DIR"
-#      mkdir $LAMMPS_BUILD_DIR/lib
-#    fi
-#    mv $LAMMPS_BUILD_DIR/lib*.a $LAMMPS_BUILD_DIR/lib
-#    techo "placeholder for findContribPackage" >> $LAMMPS_BUILD_DIR/lib/liblammps.a
+    # waitAction lammps-$BLDTYPE
+    # if ! test -d $LAMMPS_BUILD_DIR/lib; then
+    #    echo "creating directory $LAMMPS_BUILD_DIR"
+    #    mkdir $LAMMPS_BUILD_DIR/lib
+    # fi
+
   fi
 }
 
@@ -146,7 +139,7 @@ makeLammps() {
 
 putLammps() {
 
-  echo "Calling putLammps with $1"
+  techo -2 "Calling putLammps with $1"
 
   local builddir
   # If there was a build, the builddir was set
@@ -156,68 +149,62 @@ putLammps() {
   local verval=`deref $vervar`
   local BLDTYPE=$1
   local LAMMPS_INSTALL_NAME=lammps-${LAMMPS_BLDRVERSION}-$BLDTYPE
+
   # see if lammps build was attempted
   if test -z "$builddir"; then
-    techo "Not installing lammps-$verval-$1 since not built."
-# Check for previous installation
+    techo -2 "Not installing lammps-$verval-$1 since not built."
+    # Check for previous installation
     if isInstalled -i $CONTRIB_DIR $LAMMPS_INSTALL_NAME; then
-# Still make lammps find-able
-      findContribPackage lammps lammps $BLDTYPE
+      # Still make lammps find-able
+      # findContribPackage lammps lammps $BLDTYPE
+      echo "Yes LAMMPS installed"
     else
-      techo "WARNING: $LAMMPS_INSTALL_NAME not found, and not installing"
+      techo -2 "WARNING: $LAMMPS_INSTALL_NAME not found, and not installing"
     fi
     return 1
   fi
-# Install any patch
-  local patchvar=`genbashvar lammps`_PATCH
-  local patchval=`deref $patchvar`
-  if test -n "$patchval"; then
-    patchname=`basename $patchval`
-    cmd="/usr/bin/install -m 664 $patchval $CONTRIB_DIR/$LAMMPS_INSTALL_NAME/$patchname"
-    techo "$cmd"
-    $cmd
-  fi
 
-# wait for build to complete
-  waitBuild lammps-$1
-# if build was successful, then continue
+  # Install any patch
+  echo "Installing no patches for lammps-$BLDTYPE"
+
+  # wait for build to complete
+  waitAction lammps-$1
+
+  # if build was successful, then continue
   resvarname=`genbashvar lammps_$1`_RES
   local res=`deref $resvarname`
   if test "$res" != 0; then
-    techo "Not installing lammps-$verval-$1 since did not build."
+    techo -2 "Not installing lammps-$verval-$1 since did not build."
     return 1
   fi
   echo "lammps-$verval-$1 was built."
-
   local LAMMPS_INSTALL_TAG=$CONTRIB_DIR/lammps-$LAMMPS_BLDRVERSION
   local LAMMPS_INSTALL_DIR=${LAMMPS_INSTALL_TAG}-$BLDTYPE
-# Check/create install directory
+
+  # Check/create install directory
   if ! test -d $LAMMPS_INSTALL_DIR; then
     mkdir $LAMMPS_INSTALL_DIR
   fi
 
-# Make lammps includes user-writable. Not sure why this isn't being
-#  done by Lammps GNUMakefile
-  chmod -R u+w $builddir/include
+  # Check/create install bin directory
+  if ! test -d $LAMMPS_INSTALL_DIR/bin; then
+    mkdir $LAMMPS_INSTALL_DIR/bin
+  fi
 
-# Install command (if not build this time LAMMPS_BUILD_DIR fails)
-  LAMMPS_INSTTARG="lib util include"
-  for targ in $LAMMPS_INSTTARG; do
-    cmd="cp -R $builddir/$targ $LAMMPS_INSTALL_DIR"
-    echo "$cmd"
-    $cmd
-  done
+  # Install command (if not build this time LAMMPS_BUILD_DIR fails)
+  LAMMPS_INSTTARG="lmp_mac"
+  cmd="cp -R $builddir/$LAMMPS_INSTTARG $LAMMPS_INSTALL_DIR/bin"
+  techo -2 "$cmd"
+  $cmd
+  echo "Default is to copy executable into $LAMMPS_INSTALL_DIR/bin"
 
   # Register install
   ${PROJECT_DIR}/bilder/setinstald.sh -i $CONTRIB_DIR lammps,$BLDTYPE
 
-  # Make default soft links and register install
+  # Make default soft links
   if ! test -d $CONTRIB_DIR/lammps-$BLDTYPE; then
-    echo "creating soft link directory $CONTRIB_DIR/lammps-$BLDTYPE"
+    techo "creating soft link directory $CONTRIB_DIR/lammps-$BLDTYPE"
     ln -s $LAMMPS_INSTALL_DIR $CONTRIB_DIR/lammps-$BLDTYPE
   fi
-
-  # Make lammps find-able
-  findContribPackage lammps lammps $BLDTYPE
 
 }
