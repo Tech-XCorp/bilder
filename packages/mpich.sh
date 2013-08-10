c02217517323247630ac80d91d6d3f36f929de9b
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2013-08-10 17:21:43 +0000

    Trying a build of mpich-3, which has non blocking collectives.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@360 a9e16012-fd73-49ee-95cb-036de7809213

---
+++ packages/mpich.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+# Version and build information for mpich
+#
+# $Id$
+#
+######################################################################
+
+######################################################################
+#
+# Version
+#
+######################################################################
+
+if test -z "$MPICH_BLDRVERSION"; then
+  MPICH_BLDRVERSION=3.0.4
+fi
+
+######################################################################
+#
+# Builds, deps, mask, auxdata, paths, builds of other packages
+#
+######################################################################
+
+MPICH_BUILDS=${MPICH_BUILDS:-"static"}
+MPICH_DEPS=libtool,automake
+MPICH_UMASK=002
+# Not adding for now to not conflict with openmpi
+# addtopathvar PATH $CONTRIB_DIR/mpich/bin
+
+######################################################################
+#
+# Launch mpich builds.
+#
+######################################################################
+
+buildMpich() {
+
+# Unpack
+  if ! bilderUnpack mpich; then
+    exit 1
+  fi
+# Needed?
+  # MPICH_ADDL_ARGS="--enable-romio --enable-smpcoll --with-device=ch3:ssm --with-pm=hydra--with-mpe"
+# Configure and build
+  if bilderConfig mpich static "--enable-static --disable-shared $CONFIG_COMPILERS_SER $CONFIG_COMPFLAGS_SER $MPICH_ADDL_ARGS $MPICH_OTHER_ARGS"; then
+    bilderBuild mpich static
+  fi
+}
+
+######################################################################
+#
+# Test mpich
+#
+######################################################################
+
+testMpich() {
+  techo "Not testing mpich."
+}
+
+######################################################################
+#
+# Install mpich
+#
+######################################################################
+
+# Set umask to allow only group to use
+installMpich() {
+  bilderInstall mpich static
+}
+
