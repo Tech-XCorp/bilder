ef4c7553b5c920e8483b8e8e82b99c001fca6be4
jacobrking <jacobrking@a9e16012-fd73-49ee-95cb-036de7809213>
2013-08-22 21:44:47 +0000

    adding grin build script
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@372 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/nimdevel.sh
+++ packages/nimdevel.sh
@@ -21,8 +21,8 @@
 ######################################################################
 
 NIMDEVEL_BUILDS=${NIMDEVEL_BUILDS:-"ser,par"}
-
-NIMDEVEL_DEPS=fluxgrid,openmpi,superlu_dist,superlu,cmake
+NIMDEVEL_DEPS=fluxgrid,openmpi,superlu_dist,superlu,cmake,grin
+NIMDEVEL_UMASK=002
 
 ######################################################################
 #
