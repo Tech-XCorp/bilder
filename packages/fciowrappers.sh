48f766483667fa95bfe83c3882b4dc12a69fcf23
jacobrking <jacobrking@a9e16012-fd73-49ee-95cb-036de7809213>
2013-12-17 20:06:32 +0000

    version bump
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@608 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/fciowrappers.sh
+++ packages/fciowrappers.sh
@@ -12,7 +12,7 @@
 #
 ######################################################################
 
-FCIOWRAPPERS_BLDRVERSION=${FCIOWRAPPERS_BLDRVERSION:-"1.1.1-r172"}
+FCIOWRAPPERS_BLDRVERSION=${FCIOWRAPPERS_BLDRVERSION:-"1.1.1-r173"}
 
 ######################################################################
 #
