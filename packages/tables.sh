b5d3eb4b2a5bf2a15972c27d70a319899692f6a3
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2013-12-19 21:15:40 +0000

    Fix numexpr version.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@621 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/tables.sh
+++ packages/tables.sh
@@ -12,9 +12,8 @@
 #
 ######################################################################
 
-if test -z "$TABLES_BLDRVERSION"; then
-  TABLES_BLDRVERSION=2.3.1
-fi
+TABLES_BLDRVERSION_STD=2.3.1
+TABLES_BLDRVERSION_EXP=3.0.0
 
 ######################################################################
 #
