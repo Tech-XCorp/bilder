a4e89a8cf3d3d2eb5804c3db69d9fc58a28b5aa5
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2013-12-20 23:13:05 +0000

    Minor cleaning.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@635 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/cmake.sh
+++ packages/cmake.sh
@@ -13,24 +13,17 @@
 ######################################################################
 
 case `uname` in
-  CYGWIN*)
-    # fortran binding issues with 2.8.10.1 on windows
-    CMAKE_BLDRVERSION_STD=2.8.9
-    CMAKE_BLDRVERSION_EXP=2.8.12.1
-    ;;
+# fortran binding issues with 2.8.10.1 on windows
+  CYGWIN*) CMAKE_BLDRVERSION_STD=2.8.9;;
   Darwin)
     case `uname -r` in
-      13.0.*)
-        CMAKE_BLDRVERSION_STD=2.8.12.1;; # Mavericks requires 2.8.12.1
-      *)
-        CMAKE_BLDRVERSION_STD=2.8.10.1;; # other Mac OS X versions 
+      13.0.*) CMAKE_BLDRVERSION_STD=2.8.12.1;; # Mavericks requires 2.8.12.1
+      *) CMAKE_BLDRVERSION_STD=2.8.10.1;; # other Mac OS X versions
     esac
-    CMAKE_BLDRVERSION_EXP=2.8.12.1;;
-  *)
-    CMAKE_BLDRVERSION_STD=2.8.10.1
-    CMAKE_BLDRVERSION_EXP=2.8.12.1
     ;;
+  *) CMAKE_BLDRVERSION_STD=2.8.10.1;;
 esac
+CMAKE_BLDRVERSION_EXP=${CMAKE_BLDRVERSION_EXP:-"2.8.12.1"}
 
 
 ######################################################################
