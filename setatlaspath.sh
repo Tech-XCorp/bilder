6ac4b6af62ab23291665aecc59bd41da7dc1eede
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2013-12-27 15:11:56 +0000

    Adding a script to fix windows path on cygwin as needed by atlas maybe.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@680 a9e16012-fd73-49ee-95cb-036de7809213

---
+++ setatlaspath.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Source this file to fix the windows path not to have parens
+# Assume one has the mount,
+# "C:/Program Files (x86)" /ProgramFilesX86 ntfs binary 0 0
+echo $PATH >path.txt
+cat >path.sed <<EOF
+s/:/:\
+/g
+s?cygdrive/c/Program Files (x86)?ProgramFilesX86?g
+EOF
+sed -f path.sed <path.txt >path.tmp
+grep -v '(' path.tmp | tr -d '\n' >path.txt
+rm path.tmp
+PATH=`cat path.txt`
+echo PATH = $PATH
