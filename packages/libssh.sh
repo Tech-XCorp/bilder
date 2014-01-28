72c566ed294e3a7d4f90c5bb9a4a315aef61447c
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2014-01-28 16:11:15 +0000

    libssh.sh: Updating format for global vars and build stanza.  Adding latest version to experimental build.  Simplify installation with bilderInstallAll.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@869 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/libssh.sh
+++ packages/libssh.sh
@@ -12,7 +12,8 @@
 #
 ######################################################################
 
-LIBSSH_BLDRVERSION=${LIBSSH_BLDRVERSION:-"0.5.4"}
+LIBSSH_BLDRVERSION_STD=${LIBSSH_BLDRVERSION:-"0.5.4"}
+LIBSSH_BLDRVERSION_EXP=${LIBSSH_BLDRVERSION:-"0.6.0"}
 
 ######################################################################
 #
@@ -20,18 +21,20 @@ LIBSSH_BLDRVERSION=${LIBSSH_BLDRVERSION:-"0.5.4"}
 #
 ######################################################################
 
+setLibsshGlobalVars() {
 # libssh always builds the shared libs.  With configuration below,
 # it will also build the static library, so we call this build ser.
-LIBSSH_DESIRED_BUILDS=${LIBSSH_DESIRED_BUILDS:-"ser"}
-computeBuilds libssh
+  LIBSSH_DESIRED_BUILDS=${LIBSSH_DESIRED_BUILDS:-"ser"}
+  computeBuilds libssh
 # Since libssh always builds the shared libs in ser, addCc4py logic
 # not right.
-if ! isCcCc4py; then
-  addCc4pyBuild libssh
-fi
-
-LIBSSH_DEPS=openssl,cmake,zlib
-LIBSSH_UMASK=002
+  if ! isCcCc4py; then
+    addCc4pyBuild libssh
+  fi
+  LIBSSH_DEPS=openssl,cmake,zlib
+  LIBSSH_UMASK=002
+}
+setLibsshGlobalVars
 
 ######################################################################
 #
@@ -40,26 +43,34 @@ LIBSSH_UMASK=002
 ######################################################################
 
 buildLibssh() {
-  if bilderUnpack libssh; then
-    local buildargs=
-    case `uname` in
-      CYGWIN*)
-# Some failures with jom on focus
-        buildargs="-m nmake"
-        local zlibdir=`cygpath -am $CONTRIB_DIR/zlib-${ZLIB_BLDRVERSION}-sersh`
-        LIBSSH_SER_ADDL_ARGS="-DZLIB_INCLUDE_DIR:PATH=$zlibdir/include -DZLIB_LIBRARY:PATH=$zlibdir/lib/zlib.lib"
-        ;;
-      Linux)
-        LIBSSH_SER_ADDL_ARGS="  -DCMAKE_SHARED_LINKER_FLAGS:STRING=-Wl,--allow-multiple-definition -DCMAKE_EXE_LINKER_FLAGS:STRING=-Wl,--allow-multiple-definition"
-        ;;
-    esac
-    if bilderConfig -c libssh ser "-DWITH_STATIC_LIB:BOOL=TRUE $CMAKE_COMPILERS_SER $CMAKE_COMPFLAGS_SER $TARBALL_NODEFLIB_FLAGS $LIBSSH_SER_ADDL_ARGS $LIBSSH_SER_OTHER_ARGS"; then
-      bilderBuild $buildargs libssh ser
-    fi
-    if bilderConfig -c libssh cc4py "$CMAKE_COMPILERS_PYC $CMAKE_COMPFLAGS_PYC $TARBALL_NODEFLIB_FLAGS $LIBSSH_SER_ADDL_ARGS $LIBSSH_CC4PY_OTHER_ARGS"; then
-      bilderBuild $buildargs libssh cc4py
-    fi
+
+# Unpack if build needed
+  if ! bilderUnpack libssh; then
+    return
   fi
+
+# Construct configure and build args
+  local buildargs=
+  case `uname` in
+    CYGWIN*)
+# Some failures with jom on focus
+      buildargs="-m nmake"
+      local zlibdir=`cygpath -am $CONTRIB_DIR/zlib-${ZLIB_BLDRVERSION}-sersh`
+      LIBSSH_ALL_ADDL_ARGS="-DZLIB_INCLUDE_DIR:PATH=$zlibdir/include -DZLIB_LIBRARY:PATH=$zlibdir/lib/zlib.lib"
+      ;;
+    Linux)
+      LIBSSH_ALL_ADDL_ARGS="-DCMAKE_SHARED_LINKER_FLAGS:STRING=-Wl,--allow-multiple-definition -DCMAKE_EXE_LINKER_FLAGS:STRING=-Wl,--allow-multiple-definition"
+      ;;
+  esac
+
+# configure and launch builds
+  if bilderConfig -c libssh ser "-DWITH_STATIC_LIB:BOOL=TRUE $CMAKE_COMPILERS_SER $CMAKE_COMPFLAGS_SER $LIBSSH_ALL_ADDL_ARGS $LIBSSH_SER_OTHER_ARGS"; then
+    bilderBuild $buildargs libssh ser
+  fi
+  if bilderConfig -c libssh cc4py "$CMAKE_COMPILERS_PYC $CMAKE_COMPFLAGS_PYC $LIBSSH_ALL_ADDL_ARGS $LIBSSH_CC4PY_OTHER_ARGS"; then
+    bilderBuild $buildargs libssh cc4py
+  fi
+
 }
 
 ######################################################################
@@ -79,8 +90,6 @@ testLibssh() {
 ######################################################################
 
 installLibssh() {
-  for bld in `echo $LIBSSH_BUILDS | tr ',' ' '`; do
-    bilderInstall -r libssh $bld
-  done
+  bilderInstallAll libssh "-r"
 }
 
