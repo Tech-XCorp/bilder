3acac791f9fca719ffc84e384401a2b253b88613
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2014-03-16 15:48:41 +0000

    Upgrade qt to 4.8.6 snapshot to try to fix shiboken snapshot.  Exp only.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1155 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/qt.sh
+++ packages/qt.sh
@@ -32,15 +32,14 @@
 case `uname`-`uname -r` in
   Darwin-13.*)
 # 4.8.4 and 4.8.5 do not build on Mavericks
-# 4.8.6 likely not available until Jan 2014. For now install with homebrew.
-    QT_BLDRVERSION_STD=${QT_BLDRVERSION_STD:-"4.8.6"}
-    QT_BLDRVERSION_EXP=${QT_BLDRVERSION_STD:-"4.8.6"}
+# 4.8.6 snapshot below.
+    QT_BLDRVERSION_STD=${QT_BLDRVERSION_STD:-"4.8.6-20140310-510"}
     ;;
   *)
     QT_BLDRVERSION_STD=${QT_BLDRVERSION_STD:-"4.8.5"}
-    QT_BLDRVERSION_EXP=${QT_BLDRVERSION_EXP:-"4.8.5"}
     ;;
 esac
+QT_BLDRVERSION_EXP=${QT_BLDRVERSION_EXP:-"4.8.6-20140310-510"}
 
 ######################################################################
 #
