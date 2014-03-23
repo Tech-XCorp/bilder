f8023a81215ab2b9af893566aee91d9682dc565a
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2014-03-23 16:42:20 +0000

    Clean up package file.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1177 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/setuptools.sh
+++ packages/setuptools.sh
@@ -21,9 +21,12 @@ SETUPTOOLS_BLDRVERSION_EXP=${SETUPTOOLS_BLDRVERSION_EXP:-"2.0.2"}
 #
 ######################################################################
 
-SETUPTOOLS_BUILDS=${SETUPTOOLS_BUILDS:-"cc4py"}
-SETUPTOOLS_DEPS=Python
-SETUPTOOLS_UMASK=002
+setSetupToolsGlobalVars() {
+  SETUPTOOLS_BUILDS=${SETUPTOOLS_BUILDS:-"cc4py"}
+  SETUPTOOLS_DEPS=Python
+  SETUPTOOLS_UMASK=002
+}
+setSetupToolsGlobalVars
 
 #####################################################################
 #
@@ -38,15 +41,7 @@ buildSetuptools() {
     return 1
   fi
 
-# Remove all old installations
-# Is this necessary?
-  # cmd="rmall ${PYTHON_SITEPKGSDIR}/setuptools*"
-  # techo "$cmd"
-  # $cmd
-
 # Build away
-  # SETUPTOOLS_ENV="$DISTUTILS_ENV $SETUPTOOLS_GFORTRAN"
-# Does setuptools need gfortran?
   SETUPTOOLS_ENV="$DISTUTILS_ENV"
   techo -2 SETUPTOOLS_ENV = $SETUPTOOLS_ENV
   bilderDuBuild setuptools '-' "$SETUPTOOLS_ENV"
@@ -70,21 +65,8 @@ testSetuptools() {
 ######################################################################
 
 installSetuptools() {
-  local res=
   mkdir -p $PYTHON_SITEPKGSDIR
-  case `uname` in
-    CYGWIN*)
-      # mkdir -p $CONTRIB_DIR/Lib/site-packages
-      bilderDuInstall setuptools " " "$SETUPTOOLS_ENV"
-      res=$?
-      ;;
-    *)
-      # mkdir -p $PYTHON_SITEPKGSDIR
-      bilderDuInstall setuptools "--install-purelib=$PYTHON_SITEPKGSDIR" "$SETUPTOOLS_ENV"
-      res=$?
-      ;;
-  esac
-  if test $res = 0; then
+  if bilderDuInstall setuptools " " "$SETUPTOOLS_ENV"; then
     chmod a+r $PYTHON_SITEPKGSDIR/site.py*
   fi
 }
