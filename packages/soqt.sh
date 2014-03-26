5ac7cf34c33aef1d3254d80b32275bfdefd7b116
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2014-03-26 14:12:25 +0000

    Build soqt with cmake or not.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1190 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/soqt.sh
+++ packages/soqt.sh
@@ -13,6 +13,7 @@
 ######################################################################
 
 SOQT_BLDRVERSION=${SOQT_BLDRVERSION:-"1.5.0"}
+# Repo and tarball have different capitalization
 if $COIN_USE_REPO; then
   SOQT_NAME=soqt
 else
@@ -33,11 +34,11 @@ setSoQtGlobalVars() {
       SOQT_BUILDS=${SOQT_BUILDS},${FORPYTHON_BUILD}dbg
     fi
   fi
-# Repo and tarball have different capitalization
-  SOQT_DEPS=coin,Coin,qt
+  SOQT_DEPS=coin,qt
   SOQT_UMASK=002
   SOQT_REPO_URL=https://bitbucket.org/Coin3D/soqt
   SOQT_UPSTREAM_URL=https://bitbucket.org/Coin3D/soqt
+  # SOQT_CMAKE_ARG=-c # Someday?
 }
 setSoQtGlobalVars
 
@@ -80,8 +81,9 @@ buildSoQt() {
       techo "$cmd"
       eval "$cmd"
     fi
-    # rm -f $PROJECT_DIR/soqt/*-soqt-preconfig.txt
-    if ! bilderPreconfig -p : soqt; then
+# If not using cmake, do not preconfig
+    SOQT_PRECONFIG_ARGS=${SOQT_CMAKE_ARGS:-"-p :"}
+    if ! bilderPreconfig $SOQT_PRECONFIG_ARGS soqt; then
       return
     fi
 
@@ -98,33 +100,40 @@ buildSoQt() {
   local SOQT_ADDL_ARGS=
   local BASE_CC=`basename "$CC"`
   local BASE_CXX=`basename "$CXX"`
-  local SOQT_COMPILERS=
-  case `uname` in
-    CYGWIN*)
-      SOQT_ADDL_ARGS="$SOQT_ADDL_ARGS --with-msvcrt=/md"
-      SOQT_DBG_ADDL_ARGS="$SOQT_DBG_ADDL_ARGS --with-msvcrt=/mdd"
-      SOQT_COMPILERS="CC='' CXX=''"
-      ;;
-    Darwin)
-      SOQT_ADDL_ARGS="$SOQT_ADDL_ARGS --without-framework"
-      SOQT_COMPILERS="CC='$BASE_CC' CXX='$BASE_CXX'"
-      ;;
-    *)
-      SOQT_COMPILERS="CC='$BASE_CC' CXX='$BASE_CXX'"
-      ;;
-  esac
-  local SOQT_CFLAGS="$PYC_CFLAGS"
-  local SOQT_CXXFLAGS="$PYC_CXXFLAGS"
-  if [[ "$BASE_CC" =~ gcc ]]; then
-    SOQT_CFLAGS="$SOQT_CFLAGS -fpermissive"
-    SOQT_CXXFLAGS="$SOQT_CXXFLAGS -fpermissive"
+  local COMPILERS_SOQT=
+  local SOQT_CFLAGS=
+  local SOQT_CXXFLAGS=
+  local SOQT_MAKER_ARGS=
+  local SOQT_ENV=
+  if test -z "$COIN_CMAKE_ARGS"; then
+    case `uname` in
+      CYGWIN*)
+        SOQT_ADDL_ARGS="$SOQT_ADDL_ARGS --with-msvcrt=/md"
+        SOQT_DBG_ADDL_ARGS="$SOQT_DBG_ADDL_ARGS --with-msvcrt=/mdd"
+        SOQT_ENV="CC='' CXX=''"
+        SOQT_MAKER_ARGS="-m make"
+        ;;
+      Darwin)
+        SOQT_ADDL_ARGS="$SOQT_ADDL_ARGS --without-framework"
+        SOQT_ENV="CC='$BASE_CC' CXX='$BASE_CXX'"
+        ;;
+      *)
+        SOQT_ENV="CC='$BASE_CC' CXX='$BASE_CXX'"
+        ;;
+    esac
+    SOQT_CFLAGS="$PYC_CFLAGS"
+    SOQT_CXXFLAGS="$PYC_CXXFLAGS"
+    if [[ "$BASE_CC" =~ gcc ]]; then
+      SOQT_CFLAGS="$SOQT_CFLAGS -fpermissive"
+      SOQT_CXXFLAGS="$SOQT_CXXFLAGS -fpermissive"
+    fi
+    trimvar SOQT_CFLAGS ' '
+    trimvar SOQT_CXXFLAGS ' '
+  else
+    COMPILERS_SOQT="$CMAKE_COMPILERS_PYC $CMAKE_COMPFLAGS_PYC "
   fi
-  trimvar SOQT_CFLAGS ' '
-  trimvar SOQT_CXXFLAGS ' '
-  local otherargsvar=`genbashvar SOQT_${FORPYTHON_BUILD}`_OTHER_ARGS
-  local otherargsval=`deref ${otherargsvar}`
 
-# Set env
+# Find qt
   local SOQT_QTDIR=$QT_CC4PY_DIR
   SOQT_QTDIR=${SOQT_QTDIR:-"$QT_SERSH_DIR"}
   if test -z "$SOQT_DIR" -a -n "$QT_SER_DIR"; then
@@ -134,12 +143,12 @@ buildSoQt() {
   if test -z "$SOQT_QTDIR"; then
     techo "WARNING: Qt not found."
   fi
-  local SOQT_ENV=
-  local SOQT_DBG_ENV=
   if test -n "$SOQT_QTDIR"; then
-    SOQT_ENV="QTDIR=$SOQT_QTDIR"
-    SOQT_DBG_ENV="QTDIR=$SOQT_QTDIR"
+    SOQT_ENV="QTDIR=$SOQT_QTDIR $SOQT_ENV"
   fi
+  SOQT_DBG_ENV="$SOQT_ENV"
+
+# Find Coin
   local COIN_DIR=
   if $COIN_USE_REPO; then
     if test -d $BLDR_INSTALL_DIR/${COIN_NAME}-${FORPYTHON_BUILD}/bin; then
@@ -158,18 +167,20 @@ buildSoQt() {
       SOQT_DBG_ENV="$SOQT_DBG_ENV PATH=$COIN_DBG_BINDIR:'$PATH'"
     fi
   fi
-  SOQT_ENV="$SOQT_ENV $SOQT_COMPILERS"
   trimvar SOQT_ENV ' '
-  SOQT_DBG_ENV="$SOQT_DBG_ENV $SOQT_COMPILERS"
   trimvar SOQT_DBG_ENV ' '
 
 # Configure and build
-  if bilderConfig -p $COIN_NAME-$COIN_BLDRVERSION-$FORPYTHON_BUILD $SOQT_NAME $FORPYTHON_BUILD "CFLAGS='$SOQT_CFLAGS' CXXFLAGS='$SOQT_CXXFLAGS' $SOQT_ADDL_ARGS $otherargsval" "" "$SOQT_ENV"; then
-    bilderBuild -m make $SOQT_NAME $FORPYTHON_BUILD "" "$SOQT_ENV"
+  local otherargs=`deref SOQT_${FORPYTHON_BUILD}_OTHER_ARGS`
+  if bilderConfig -p $COIN_NAME-$COIN_BLDRVERSION-$FORPYTHON_BUILD $SOQT_NAME $FORPYTHON_BUILD "$COMPILERS_SOQT $SOQT_ADDL_ARGS $otherargs" "" "$SOQT_ENV"; then
+    bilderBuild $SOQT_MAKER_ARGS $SOQT_NAME $FORPYTHON_BUILD "" "$SOQT_ENV"
   fi
-  if bilderConfig -p $COIN_NAME-$COIN_BLDRVERSION-${FORPYTHON_BUILD}dbg $SOQT_NAME ${FORPYTHON_BUILD}dbg "CFLAGS='$SOQT_CFLAGS' CXXFLAGS='$SOQT_CXXFLAGS' $SOQT_DBG_ADDL_ARGS $otherargsval" "" "$SOQT_DBG_ENV"; then
-    bilderBuild -m make $SOQT_NAME ${FORPYTHON_BUILD}dbg "" "$SOQT_DBG_ENV"
+
+if false; then
+  if bilderConfig -p $COIN_NAME-$COIN_BLDRVERSION-${FORPYTHON_BUILD}dbg $SOQT_NAME ${FORPYTHON_BUILD}dbg "CFLAGS='$SOQT_CFLAGS' CXXFLAGS='$SOQT_CXXFLAGS' $SOQT_DBG_ADDL_ARGS $otherargs" "" "$SOQT_DBG_ENV"; then
+    bilderBuild $SOQT_MAKER_ARGS $SOQT_NAME ${FORPYTHON_BUILD}dbg "" "$SOQT_DBG_ENV"
   fi
+fi
 
 }
 
@@ -191,7 +202,7 @@ testSoQt() {
 
 installSoQt() {
   for bld in `echo $SOQT_BUILDS | tr ',' ' '`; do
-    bilderInstall -m make -L $SOQT_NAME $bld
+    bilderInstall $SOQT_MAKER_ARGS -L $SOQT_NAME $bld
   done
 }
 
