ec0a94ef12624b35f605ee2654db47c2adc8a7d2
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2014-04-03 19:06:34 +0000

    Release candidates for 4.8.6 now available.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1216 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/qtversions.sh
+++ packages/qtversions.sh
@@ -10,7 +10,8 @@ case `uname`-`uname -r` in
   Darwin-13.*)
 # 4.8.4 and 4.8.5 do not build on Mavericks
 # 4.8.6 snapshot below does.
-# Snapshots available from http://download.qt-project.org/snapshots/qt/4.8/
+# Snapshots: http://download.qt-project.org/snapshots/qt/4.8/
+# Release cands: http://download.qt-project.org/development_releases/qt/4.8/
     QT_BLDRVERSION_STD=${QT_BLDRVERSION_STD:-"4.8.6-20140310-510"}
     ;;
   *)
