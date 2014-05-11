b49eac516158b615bc53988ff491325c96b56189
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2014-05-11 15:28:17 +0000

    Updating geant4 variables.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1331 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/geant4_aux.sh
+++ packages/geant4_aux.sh
@@ -21,23 +21,26 @@ getGeant4Version
 findGeant4() {
 
 # Look for Geant4 in the contrib directory
-  findContribPackage Geant4 TKMath sersh cc4py
+  findContribPackage Geant4 G4global sersh cc4py
   findCc4pyDir Geant4
 
 # Environment variables to help find Geant4
   local GEANT4_HOME="$CONTRIB_DIR/geant4-sersh"
   GEANT4_HOME=`(cd $GEANT4_HOME; pwd -P)`
-  GEANT4_REGVER=`echo $GEANT4_BLDRVERSION | sed -e 's/\.p0*/./' -e 's/\.00*\./.0./' -e 's/\.00*/.0/'`
+  local GEANT4_REGVER=`echo $GEANT4_BLDRVERSION | sed -e 's/\.p0*/./' -e 's/\.00*\./.0./' -e 's/\.00*/.0/'`
   export G4DATA="$GEANT4_HOME/share/Geant4-${GEANT4_REGVER}/data"
   export G4LEDATA="$G4DATA/G4EMLOW6.32"
   export G4LEVELGAMMADATA="$G4DATA/PhotonEvaporation2.3"
   export G4NEUTRONXSDATA="$G4DATA/G4NEUTRONXS1.2"
   export G4SAIDXSDATA="$G4DATA/G4SAIDDATA1.1"
+  for i in G4DATA G4LEDATA G4LEVELGAMMADATA G4NEUTRONXSDATA G4SAIDXSDATA; do
+    printvar $i
+  done
 
-# The following lines introduce new env vars such as
-# G4INSTALL, G4INCLUDE etc.  Not sure if we use them as they are not cmake.
+# The following lines introduce new env vars such as G4INSTALL, G4INCLUDE etc.
+# Do these work for cmake projects?
 #  source $GEANT4_HOME/bin/geant4.sh
-#  source $GEANT4_HOME/share/Geant4-9.6.2/geant4make/geant4make.sh
+#  source $GEANT4_HOME/share/Geant4-${GEANT4_REGVER}/geant4make/geant4make.sh
 
 }
 
