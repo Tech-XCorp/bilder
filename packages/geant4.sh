bd3e5c04056c82cc21829d61af74627b37ee5a23
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2014-05-11 03:23:21 +0000

    update geant4.sh.  Put vars only where needed.  Some formatting.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1323 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/geant4.sh
+++ packages/geant4.sh
@@ -12,37 +12,27 @@
 #
 ######################################################################
 
-GEANT4_BLDRVERSION=${GEANT4_BLDRVERSION:-"9.6.p02"}
+GEANT4_BLDRVERSION=${GEANT4_BLDRVERSION:-"10.00.p01"}
 
 ######################################################################
 #
-# Other values
+# Builds, deps, mask, auxdata, paths, builds of other packages
 #
 ######################################################################
 
-GEANT4_BUILDS=${GEANT4_BUILDS:-"ser"}
-GEANT4_DEPS=cmake,pcre,xercesc
-
-case `uname` in
-  CYGWIN*)
-    ;;
-  Darwin)
-    ;;
-  Linux)
-    GEANT4_ARGS="${GEANT4_ARGS} -DGEANT4_USE_SYSTEM_EXPAT:BOOL=OFF"
-    ;;
-esac
-
-GEANT4_ARGS="${GEANT4_ARGS} -DGEANT4_USE_QT:BOOL=ON -DGEANT4_USE_OPENGL_X11:BOOL=ON -DGEANT4_INSTALL_DATA=ON -DGEANT4_USE_GDML:BOOL=ON -DXERCESC_ROOT_DIR:PATH='$CONTRIB_DIR/xercesc'"
-
-######################################################################
-#
-# Add to path
-#
-######################################################################
-
-addtopathvar PATH $CONTRIB_DIR/geant4/bin
-
+setGeant4GlobalVars() {
+  GEANT4_BUILDS=${GEANT4_BUILDS:-"ser"}
+  GEANT4_DEPS=cmake,pcre,xercesc
+  case `uname` in
+    CYGWIN*) ;;
+    Darwin) ;;
+    Linux)
+      GEANT4_ARGS="${GEANT4_ARGS} -DGEANT4_USE_SYSTEM_EXPAT:BOOL=OFF"
+      ;;
+  esac
+  addtopathvar PATH $CONTRIB_DIR/geant4/bin
+}
+setGeant4GlobalVars
 
 ######################################################################
 #
@@ -52,6 +42,7 @@ addtopathvar PATH $CONTRIB_DIR/geant4/bin
 
 buildGeant4() {
   if bilderUnpack geant4; then
+    GEANT4_ARGS="${GEANT4_ARGS} -DGEANT4_USE_QT:BOOL=ON -DGEANT4_USE_OPENGL_X11:BOOL=ON -DGEANT4_INSTALL_DATA=ON -DGEANT4_USE_GDML:BOOL=ON -DXERCESC_ROOT_DIR:PATH='$CONTRIB_DIR/xercesc'"
     if bilderConfig -c geant4 ser "$GEANT4_ARGS $CMAKE_SUPRA_SP_ARG"; then
       bilderBuild geant4 ser ""
     fi
@@ -76,13 +67,12 @@ testGeant4() {
 
 installGeant4() {
   bilderInstall -r geant4 ser geant4
-   local GEANT4_HOME="$BLDR_INSTALL_DIR/geant4"
-   export G4DATA="$GEANT4_HOME/share/Geant4-9.6.2/data"
-   export G4LEDATA="$G4DATA/G4EMLOW6.32"
-   export G4LEVELGAMMADATA="$G4DATA/PhotonEvaporation2.3"
-   export G4NEUTRONXSDATA="$G4DATA/G4NEUTRONXS1.2"
-   export G4SAIDXSDATA="$G4DATA/G4SAIDDATA1.1"
-
+  local GEANT4_HOME="$BLDR_INSTALL_DIR/geant4"
+  export G4DATA="$GEANT4_HOME/share/Geant4-9.6.2/data"
+  export G4LEDATA="$G4DATA/G4EMLOW6.32"
+  export G4LEVELGAMMADATA="$G4DATA/PhotonEvaporation2.3"
+  export G4NEUTRONXSDATA="$G4DATA/G4NEUTRONXS1.2"
+  export G4SAIDXSDATA="$G4DATA/G4SAIDDATA1.1"
 # The following lines introduce new env vars such as
 # G4INSTALL, G4INCLUDE etc.  Not sure if we use them as they are no cmake.
 #  . $GEANT4_HOME/bin/geant4.sh
