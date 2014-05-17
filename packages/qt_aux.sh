d10f337979cc1063d51af187d119a099015e162d
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2014-05-17 14:04:05 +0000

    A more regular approach to finding qt.  Removed findQt at bottom, as non-built packages are now included in the build chain.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1385 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/qt_aux.sh
+++ packages/qt_aux.sh
@@ -29,11 +29,36 @@ getQtVersion() {
 getQtVersion
 
 #
+# print Qt vars
+#
+printQtVars() {
+  local qtvars="QMAKE QTDIR QT_BINDIR"
+  for i in $qtvars; do
+    printvar $i
+  done
+}
+
+#
 # Find the QT packages
 #
 findQt() {
 
-# First try to find Qt in the contrib directory
+# Try accepting what the user specified
+  if test -n "$QT_BINDIR"; then
+    if QMAKE=`env PATH=$QT_BINDIR:/usr/bin which qmake 2>/dev/null`; then
+      techo "qmake found in $QT_BINDIR"
+      addtopathvar PATH $QT_BINDIR
+      QTDIR=`dirname $QT_BINDIR`
+      printQtVars
+      return
+    else
+      unset QT_BINDIR
+      unset QTDIR
+    fi
+  fi
+  techo "Qt not found in $QT_BINDIR."
+
+# Seek Qt in the contrib directory
   local libname=
   if [[ `uname` =~ CYGWIN ]]; then
     libname=QtCore4
@@ -48,51 +73,43 @@ findQt() {
   local qtdir=$QT_CC4PY_DIR
   qtdir=${qtdir:-"$QT_SER_DIR"}
   if test -n "$qtdir"; then
-    QT_BINDIR=$qtdir/bin
-    QMAKE=$qtdir/qmake
     techo "Qt found in $CONTRIB_DIR."
-    printvar QT_BINDIR
+    QT_BINDIR=$qtdir/bin
     addtopathvar PATH $QT_BINDIR
-    techo "$QT_BINDIR added to path."
-    return 0
+    QMAKE=`env PATH=$QT_BINDIR:/usr/bin which qmake 2>/dev/null`
+    QTDIR=$qtdir
+    printQtVars
+    return
+  fi
+  techo "Qt not found in $CONTRIB_DIR."
+
+# Seek qmake in one's path
+  if QMAKE=`which qmake 2>/dev/null`; then
+    QT_BINDIR=`dirname $QMAKE`
+    addtopathvar PATH $QT_BINDIR
+    QTDIR=`dirname $QT_BINDIR`
+    printQtVars
+    return
+  fi
+  techo "qmake not found in the user path = $PATH."
+
+# Look in standard windows installation dirs
+  if [[ `uname` =~ CYGWIN ]]; then
+    techo "Searching Windows installation directories."
+    QT_BVER=`ls /cygdrive/c/Qt | grep "^4" | tail -1`
+    if test -n "$QT_BVER"; then
+      QT_BINDIR=/cygdrive/c/Qt/$QT_BVER/bin
+      addtopathvar PATH $QT_BINDIR
+      QMAKE=`env PATH=$QT_BINDIR which qmake 2>/dev/null`
+      QTDIR=`dirname $QT_BINDIR`
+      printQtVars
+      return
+    fi
+    techo "Qt not found in the Windows standard installation areas."
   fi
 
-  techo "Qt not found in $CONTRIB_DIR."
-# Next try to find qmake in one's path
-  QMAKE=`which qmake 2>/dev/null`
-  if test -n "$QMAKE"; then
-    QT_BINDIR=`dirname $QMAKE`
-    techo "qmake found in PATH.  Using QT_BINDIR = $QT_BINDIR."
-    return 0
-  fi
-  techo "qmake not in path."
-  if test -z "$QT_BINDIR"; then
-    case `uname` in
-      CYGWIN*)
-        techo "Searching Windows installation directories."
-        QT_BVER=`ls /cygdrive/c/Qt | grep "^4" | tail -1`
-        if test -n "$QT_BVER"; then
-          QT_BINDIR=/cygdrive/c/Qt/$QT_BVER/bin
-        fi
-        ;;
-    esac
-  fi
-  if test -z "$QT_BINDIR"; then
-    techo "WARNING: [$FUNCNAME] Could not find Qt."
-    return 1
-  fi
-  QMAKE=$QT_BINDIR/qmake
-  techo "QT_BINDIR = $QT_BINDIR."
-  addtopathvar PATH $QT_BINDIR
-  local qtvars="QMAKE QTDIR QT_BINDIR"
-  for i in $qtvars; do
-    printvar $i
-  done
+# Out of ideas
+  techo "findQt could not find Qt."
   return 0
 }
-
-#
-# Looks like we have to find qt at sourcing
-#
-findQt
 
