99f158cf3bfd37e6cc4686db14e6da3a309bd063
techxdws <techxdws@a9e16012-fd73-49ee-95cb-036de7809213>
2014-06-16 22:42:13 +0000

    Regularizing list of directions to be checked for Freetype.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1556 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/freetype_aux.sh
+++ packages/freetype_aux.sh
@@ -15,6 +15,8 @@
 #
 ######################################################################
 
+DIRLIST="/opt/homebrew /opt/homebrew/opt/freetype /opt/X11 /usr/X11R6 /usr/X11"
+
 findFreetype() {
 
 # Parse options
@@ -34,7 +36,7 @@ findFreetype() {
 
 # OSX puts freetype under the X11 location, which may be in more than one place.
   if test -z "$FREETYPE_SERSH_DIR"; then
-    for dir in /opt/homebrew /opt/homebrew/opt/freetype /opt/X11 /usr/X11R6 /usr/X11; do
+    for dir in $DIRLIST; do
       if test -d $dir/include/freetype2; then
         FREETYPE_CC4PY_DIR=$dir
         break
@@ -122,7 +124,7 @@ findFreetypeRootdir() {
   fi
 # OSX puts freetype under the X11 location, which may be in more than one place.
   if test -z "$ftdir"; then
-    for dir in /opt/homebrew/opt/freetype /opt/X11 /usr/X11R6 /usr /opt/homebrew /opt/local; do
+    for dir in $DIRLIST; do
       if test -d $dir/include/freetype2; then
         ftdir=$dir
         break
