94970c7975faa0806c281d68c705bb3807597f53
cperry87 <cperry87@a9e16012-fd73-49ee-95cb-036de7809213>
2014-07-09 19:44:29 +0000

    Setting up conditions so that cmake 2.8.10.1 is used on OSX machines with CUDA until the duplicate RPATH issue is resolved (2.8.12.x and 3.0.0 didn't solve these problems).
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1626 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/cmake_aux.sh
+++ packages/cmake_aux.sh
@@ -16,7 +16,15 @@
 ######################################################################
 
 setCmakeTriggerVars() {
-  CMAKE_BLDRVERSION_STD=${CMAKE_BLDRVERSION_STD:-"2.8.12.1"}
+  local stdVersion="2.8.12.1"
+  case `uname` in
+    Darwin*)
+      if test -d /usr/local/cuda; then 
+        stdVersion="2.8.10.1"
+      fi
+      ;;
+  esac
+  CMAKE_BLDRVERSION_STD=${CMAKE_BLDRVERSION_STD:-${stdVersion}}
   CMAKE_BLDRVERSION_EXP=${CMAKE_BLDRVERSION_EXP:-"3.0.0"}
   CMAKE_BUILDS=${CMAKE_BUILDS:-"ser"}
   CMAKE_DEPS=
