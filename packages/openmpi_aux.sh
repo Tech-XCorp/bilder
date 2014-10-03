e82f6d6059f3c99099ac94083c50c2297ae56473
techxdws <techxdws@a9e16012-fd73-49ee-95cb-036de7809213>
2014-10-03 17:19:14 +0000

    Updatding openmpi find to be like MPICH find, i.e., we now use the env var $USE_MPI to determine MPI type. openmpi is the default, so one only really need specify USE_MPI when using MPICH (or if one wants an openmpi build other than nodl).
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1782 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/openmpi_aux.sh
+++ packages/openmpi_aux.sh
@@ -38,12 +38,26 @@ setOpenmpiTriggerVars
 
 findOpenmpi() {
 # Obtain correct mpi compiler names after bildall.sh is called
-  addtopathvar PATH $CONTRIB_DIR/openmpi/bin
-  MPICC=`basename "$MPICC"`
-  MPICXX=`basename "$MPICXX"`
-  MPIFC=`basename "$MPIFC"`
-  MPIF77=`basename "$MPIF77"`
-  findParallelFcComps
-  getCombinedCompVars
+  if [[ "$USE_MPI" =~ openmpi ]]; then
+    addtopathvar PATH $CONTRIB_DIR/$USE_MPI/bin
+    openmpidir=`(cd $CONTRIB_DIR/$USE_MPI/bin; pwd -P)`
+    for c in MPICC MPICXX MPIFC MPIF77; do
+      case $c in
+        MPIFC) MPIFC=$openmpidir/mpif90;;
+        *) exe=`echo $c | tr [A-Z] [a-z]`; eval $c=$openmpidir/$exe;;
+      esac
+      exe=`deref $c`
+      if ! test -x $exe; then
+        techo "WARNING: $exe not found."
+      fi
+      printvar $c
+    done
+    OPENMPI_LIBDIR=`$MPICC -show | sed -e 's/^.*-L//' -e 's/ .*$//'`
+    PAR_EXTRA_LDFLAGS="$PAR_EXTRA_LDFLAGS ${RPATH_FLAG}$OPENMPI_LIBDIR"
+    PAR_EXTRA_LT_LDFLAGS="$PAR_EXTRA_LDFLAGS ${LT_RPATH_FLAG}$OPENMPI_LIBDIR"
+    printvar PAR_EXTRA_LDFLAGS
+    printvar PAR_EXTRA_LT_LDFLAGS
+    getCombinedCompVars
+  fi
 }
 
