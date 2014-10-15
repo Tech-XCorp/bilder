dd886b48e53a24161c62c534a1ddbaa87f6b9c72
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2014-10-15 00:52:21 +0000

    cgm works off of cadg4 branch.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1808 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/cgm_aux.sh
+++ packages/cgm_aux.sh
@@ -16,7 +16,7 @@
 ######################################################################
 
 setCgmTriggerVars() {
-  CGM_REPO_URL=https://bitbucket.org/fathomteam/cgm.git
+  CGM_REPO_URL=https://bitbucket.org/cadg4/cgm.git
   CGM_UPSTREAM_URL=https://bitbucket.org/fathomteam/cgm.git
   CGM_REPO_TAG_EXP=master
   CGM_BUILD=$FORPYTHON_BUILD
