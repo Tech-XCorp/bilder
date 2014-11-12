d4da6e8df1699898d9291f45ebd80db0ab808f4f
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2014-11-12 13:03:50 +0000

    Not updating detached branches.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1860 a9e16012-fd73-49ee-95cb-036de7809213

--- updategitrepos.sh
+++ updategitrepos.sh
@@ -2,9 +2,16 @@
 #
 # $Id$
 
+topdir=`pwd`
 for i in *; do
   if test -d $i/.git; then
-    echo "Updating $i."
-    (cd $i; git pull)
+    cd $i
+    if git branch | grep '^\*' | grep -q detached; then
+      echo "Not updating $i because it is detached."
+    else
+      echo "Updating $i."
+      git pull
+    fi
+    cd $topdir
   fi
 done
