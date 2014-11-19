8bfb11eb123db7de616553772f71954246362283
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2014-11-19 14:56:30 +0000

    Fix after name change.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1924 a9e16012-fd73-49ee-95cb-036de7809213

--- bildfcns.sh
+++ bildfcns.sh
@@ -3465,7 +3465,7 @@ updateRepo() {
   cmd=:
   local branchvar=
   if $BUILD_EXPERIMENTAL; then
-    branchvar=`genbashvar $1`_UPSTREAM_BRANCH
+    branchvar=`genbashvar $1`_REPO_BRANCH_EXP
   else
     branchvar=`genbashvar $1`_REPO_BRANCH_STD
   fi
