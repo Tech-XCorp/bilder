c2d4be4bbc254b1843cc001b9590031a5ca01f3d
techxdws <techxdws@a9e16012-fd73-49ee-95cb-036de7809213>
2014-11-22 00:06:02 +0000

    Reversing r1931 and r1933 as they break the Vorpal build and/or tests which use Trilinos.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1934 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/trilinos_aux.sh
+++ packages/trilinos_aux.sh
@@ -39,7 +39,7 @@ setTrilinosTriggerVars() {
   computeBuilds trilinos
 
 # Add in superlu all the time.  May be needed elsewhere
-  TRILINOS_DEPS=${TRILINOS_DEPS:-"hypre,mumps,superlu_dist,boost,$MPI_BUILD,superlu,swig,numpy,atlas,lapack"}
+  TRILINOS_DEPS=${TRILINOS_DEPS:-"superlu_dist,boost,$MPI_BUILD,superlu,swig,numpy,atlas,lapack"}
 # commio builds depend on netcdf and hdf5. Only add in if these builds are present.
   if echo "$TRILINOS_BUILDS" | grep -q "commio" ; then
     TRILINOS_DEPS="netcdf,hdf5,${TRILINOS_DEPS}"
@@ -58,75 +58,3 @@ findTrilinos() {
   :
 }
 
-getTriPackages() {
-  local triPackages=$@
-  local triPkgArgs=""
-  for pkg in $triPackages; do
-    triPkgArgs="$triPkgArgs -DTrilinos_ENABLE_${pkg}:BOOL=ON"
-  done
-  echo $triPkgArgs
-}
-mkTplLibConfig() {
-    local TPL=$1
-    local tplDir=$2
-    local tplLibName=${@:3:20}
-    local tplConfig="-D${TPL}_INCLUDE_DIRS:PATH='${tplDir}/include' -D${TPL}_LIBRARY_DIRS:PATH='${tplDir}/lib' -D${TPL}_LIBRARY_NAMES:STRING='$tplLibName'"
-    echo $tplConfig
-}
-
-getTriTPLs() {
-  local buildtype=${1}
-  local parflag=${buildtype:0:3}     # This converts sercplx to ser
-  local TPLs=${@:2:20}
-
-  local tplArgs=""
-  for TPL in $TPLs; do
-    argOn="-DTPL_ENABLE_${TPL}:BOOL=ON"
-    case "$TPL" in
-      Hypre) 
-        if test "$parflag" == "ser"; then
-          tplDir=$CONTRIB_DIR/hypre-${HYPRE_BLDRVERSION}-sersh
-        else
-          tplDir=$CONTRIB_DIR/hypre-${HYPRE_BLDRVERSION}-parsh
-        fi
-        tplLibName="HYPRE"
-        ;;
-      SuperLU) 
-        if test "$parflag" == "ser"; then
-          tplDir=$CONTRIB_DIR/superlu
-        else
-          continue  # parallel only
-        fi
-        tplLibName="superlu"
-        ;;
-      SuperLU_DIST) 
-        if test "$parflag" == "ser"; then
-          continue  # parallel only
-        else
-          tplDir=$CONTRIB_DIR/superlu_dist-par
-        fi
-        tplLibName="superlu_dist"
-        ;;
-      MUMPS)
-        if test "$parflag" == "ser"; then
-          if test -d $CONTRIB_DIR/mumps; then
-            tplDir=$CONTRIB_DIR/mumps
-          else
-            tplDir=$BLDR_INSTALL_DIR/mumps
-          fi
-        else
-          if test -d $CONTRIB_DIR/mumps-par; then
-            tplDir=$CONTRIB_DIR/mumps-par
-          else
-            tplDir=$BLDR_INSTALL_DIR/mumps-par
-          fi
-        fi
-        tplLibName='cmumps zmumps smumps dmumps mumps_common'
-        ;;
-    esac
-    local tplLibArgs=`mkTplLibConfig $TPL $tplDir $tplLibName`
-    tplArgs="$tplArgs $argOn $tplLibArgs"
-  done
-  echo $tplArgs
-  return
-}
