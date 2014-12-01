a8f7321b66ec454f1a6556270cece95853dc580c
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2014-12-01 08:36:58 +0000

    cppcheck has lots of problems.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@1961 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/txbase_aux.sh
+++ packages/txbase_aux.sh
@@ -27,7 +27,7 @@ setTxbaseTriggerVars() {
   fi
   computeBuilds txbase
   addPycshBuild txbase
-  TXBASE_DEPS=hdf5,$MPI_BUILD,boost,Python,cppcheck,cmake
+  TXBASE_DEPS=hdf5,$MPI_BUILD,boost,Python,cmake
 }
 setTxbaseTriggerVars
 
