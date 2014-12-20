c32c5040cfe62709b36ba2e334ec21f07f319dde
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2014-12-20 22:03:48 +0000

    Build graphviz on non-linux.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@2049 a9e16012-fd73-49ee-95cb-036de7809213

---
+++ packages/graphviz.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# Build information for graphviz
+# PETSc does not allow serial build w/graphviz
+#
+# $Id$
+#
+######################################################################
+
+######################################################################
+#
+# Trigger variables set in graphviz_aux.sh
+#
+######################################################################
+
+mydir=`dirname $BASH_SOURCE`
+source $mydir/graphviz_aux.sh
+
+######################################################################
+#
+# Set variables that should trigger a rebuild, but which by value change
+# here do not, so that build gets triggered by change of this file.
+# E.g: mask
+#
+######################################################################
+
+setGraphvizNonTriggerVars() {
+  GRAPHVIZ_UMASK=002
+}
+setGraphvizNonTriggerVars
+
+######################################################################
+#
+# Launch graphviz builds.
+#
+######################################################################
+
+buildGraphviz() {
+
+  if ! bilderUnpack graphviz; then
+    return 1
+  fi
+
+  if bilderConfig -c graphviz ser "$CONFIG_COMPILERS_SER $CONFIG_COMPFLAGS_PAR"; then
+    bilderBuild graphviz ser
+  fi
+
+}
+
+######################################################################
+#
+# Test graphviz
+#
+######################################################################
+
+testGraphviz() {
+  techo "Not testing graphviz."
+}
+
+######################################################################
+#
+# Install graphviz
+#
+######################################################################
+
+installGraphviz() {
+  bilderInstallAll graphviz "  -p open"
+}
+
---
+++ packages/graphviz_aux.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Trigger vars and find information
+#
+# $Id$
+#
+######################################################################
+
+######################################################################
+#
+# Set variables whose change should not trigger a rebuild or will
+# by value change trigger a rebuild, as change of this file will not
+# trigger a rebuild.
+# E.g: version, builds, deps, auxdata, paths, builds of other packages
+#
+######################################################################
+
+setGraphvizTriggerVars() {
+  GRAPHVIZ_BLDRVERSION_STD=${GRAPHVIZ_BLDRVERSION_STD:-"2.38.0"}
+  GRAPHVIZ_BLDRVERSION_EXP=${GRAPHVIZ_BLDRVERSION_EXP:-"2.38.0"}
+  if test -z "$GRAPHVIZ_BUILDS"; then
+    case `uname` in
+      CYGWIN*) ;;
+      Darwin) GRAPHVIZ_BUILDS="ser";;
+      Linux) GRAPHVIZ_BUILDS="ser";;
+    esac
+  fi
+  GRAPHVIZ_DEPS=libtool
+}
+setGraphvizTriggerVars
+
+######################################################################
+#
+# Find graphviz
+#
+######################################################################
+
+findGraphviz() {
+  :
+}
+
