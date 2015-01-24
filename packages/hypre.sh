a02345f05dc37453a84720befb2a4c116b486627
jrobcary <jrobcary@a9e16012-fd73-49ee-95cb-036de7809213>
2015-01-24 22:08:19 +0000

    Hypre was not being built correctly, needed CMAKE_INSTALL_NAME_DIR set to be able to be found later by an executable.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@2112 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/hypre.sh
+++ packages/hypre.sh
@@ -40,12 +40,19 @@ buildHypre() {
   if ! bilderUnpack hypre; then
     return 1
   fi
+  local HYPRE_PAR_ADDL_ARGS=
+  local HYPRE_PARSH_ADDL_ARGS=
+  case `uname` in
+    Darwin)
+      HYPRE_PARSH_ADDL_ARGS="-DCMAKE_INSTALL_NAME_DIR:STRING='$CONTRIB_DIR/hypre-${HYPRE_BLDRVERSION}-parsh/lib'"
+      ;;
+  esac
 
-  if bilderConfig -c hypre par "-DHYPRE_INSTALL_PREFIX:PATH=$CONTRIB_DIR/hypre-${HYPRE_BLDRVERSION}-par $CMAKE_COMPILERS_PAR $CMAKE_COMPFLAGS_PAR"; then
+  if bilderConfig -c hypre par "-DHYPRE_INSTALL_PREFIX:PATH=$CONTRIB_DIR/hypre-${HYPRE_BLDRVERSION}-par $CMAKE_COMPILERS_PAR $CMAKE_COMPFLAGS_PAR $HYPRE_PAR_ADDL_ARGS $HYPRE_PAR_OTHER_ARGS"; then
     bilderBuild hypre par
   fi
 
-  if bilderConfig -c hypre parsh "-DHYPRE_SHARED:BOOL=ON -DHYPRE_INSTALL_PREFIX:PATH=$CONTRIB_DIR/hypre-${HYPRE_BLDRVERSION}-parsh $CMAKE_COMPILERS_PAR $CMAKE_COMPFLAGS_PAR"; then
+  if bilderConfig -c hypre parsh "-DHYPRE_SHARED:BOOL=ON -DHYPRE_INSTALL_PREFIX:PATH=$CONTRIB_DIR/hypre-${HYPRE_BLDRVERSION}-parsh $CMAKE_COMPILERS_PAR $CMAKE_COMPFLAGS_PAR $HYPRE_PARSH_ADDL_ARGS $HYPRE_PARSH_OTHER_ARGS"; then
     bilderBuild hypre parsh
   fi
 
