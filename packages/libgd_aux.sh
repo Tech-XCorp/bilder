9ddff1d738979286b61d780b629d2814e46bb17c
techxdws <techxdws@a9e16012-fd73-49ee-95cb-036de7809213>
2015-02-23 14:13:05 +0000

    Reversing r2165; going back to WARNING if libgd is not found.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@2181 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/libgd_aux.sh
+++ packages/libgd_aux.sh
@@ -28,8 +28,7 @@ findLibgd() {
     *)
       local libgdconfig=`which gdlib-config`
       if test -z "$libgdconfig"; then
-        techo "ERROR: [$FUNCNAME] libgd not found by bilder. Please install gd-devel or gd-dev on Linux, or gd with homebrew on OSX."
-        terminate
+        techo "WARNING: [$FUNCNAME] libgd not found by bilder. Please install gd-devel or gd-dev on Linux, or gd with homebrew on OSX."
       else
         local libgdlibdir=`$libgdconfig --libdir`
         local libgddir=`(cd $libgdlibdir/..; pwd -P)`
