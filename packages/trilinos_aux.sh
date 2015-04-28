4d8f240633bdce57b16e81c8ffe49b9095734353
techxdws <techxdws@a9e16012-fd73-49ee-95cb-036de7809213>
2015-04-28 22:31:20 +0000

    Updating Trilinos version in this devel branch.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/branches/moab01@2329 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/trilinos_aux.sh
+++ packages/trilinos_aux.sh
@@ -23,7 +23,7 @@
 setTrilinosTriggerVars() {
 # Versions
   TRILINOS_BLDRVERSION_STD=11.4.3
-  TRILINOS_BLDRVERSION_EXP=11.12.1
+  TRILINOS_BLDRVERSION_EXP=11.14.3
 # Can add builds in package file only if no add builds defined.
   if test -z "$TRILINOS_DESIRED_BUILDS"; then
     TRILINOS_DESIRED_BUILDS="sercomm,parcomm"
