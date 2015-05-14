f41570db61402ca87ea62c6045d306274fb7526e
jacobrking <jacobrking@a9e16012-fd73-49ee-95cb-036de7809213>
2015-05-14 20:20:04 +0000

    Add intel-mic machine file for phi / babbage.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@2380 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/nimdevel.sh
+++ packages/nimdevel.sh
@@ -22,7 +22,7 @@
 
 NIMDEVEL_BUILDS=${NIMDEVEL_BUILDS:-"ser,par"}
 # don't include autotools -- for testing only
-NIMDEVEL_DEPS=$MPI_BUILD,netlib_lite,fciowrappers,superlu,cmake
+NIMDEVEL_DEPS=$MPI_BUILD,fciowrappers,superlu,cmake
 NIMDEVEL_UMASK=002
 nimversion=nimdevel
 
@@ -35,7 +35,7 @@ nimversion=nimdevel
 if $NIMDEVEL_WITH_OPENMP; then
   NIMDEVEL_DEPS=${NIMDEVEL_DEPS}",superlu_dist4" # CUDA/OpenMP enabled
 else
-  NIMDEVEL_DEPS=${NIMDEVEL_DEPS}",superlu_dist3"
+  NIMDEVEL_DEPS=${NIMDEVEL_DEPS}"netlib_lite,superlu_dist3"
 fi
 if $BUILD_DEBUG; then
   NIMDEVEL_BUILDS=${NIMDEVEL_BUILDS}",serdbg,pardbg"
