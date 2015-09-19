bdddb5d8efc1026dfc154760aaf9656b83f6e4af
cary <cary@a9e16012-fd73-49ee-95cb-036de7809213>
2015-09-19 16:25:57 +0000

    basic script for invoking jenkins parameterized build.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@2566 a9e16012-fd73-49ee-95cb-036de7809213

---
+++ jenkins/invokejenkins.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+# https://wiki.jenkins-ci.org/display/JENKINS/Parameterized+Builds
+
+usage() {
+  cat >&2 <<EOF
+Usage: invokebuild.sh [options]
+Options:
+  -b <svnbranch>.. The branch to build
+  -c <cause> ..... Jenkins puts this as the cause of the build
+  -h ............. Print this message.
+  -j <job> ....... The Jenkins job, e.g., vorpal-test
+  -t <target> .... The target for the build
+  -T <token> ..... The job remote invocation token
+  -u <baseurl> ... The base url, e.g., carys.org:8300
+EOF
+  exit
+}
+
+svnbranch=
+cause=
+job=
+target=
+token=
+baseurl=
+
+while getopts "b:c:ht:T:u:" arg; do
+  case $arg in
+    b) svnbranch=$OPTARG;;
+    c) cause=$OPTARG;;
+    h) usage;;
+    t) target=$OPTARG;;
+    T) token=$OPTARG;;
+    t) target=$OPTARG;;
+    u) baseurl=$OPTARG;;
+  esac
+done
+
+# Required parameters
+argerr=0
+if test -z "$baseurl"; then
+  echo "Must specify baseurl with -u"
+  argerr=`expr $argerr + 1`
+fi
+if test -z "$job"; then
+  echo "Must specify job with -j"
+  argerr=`expr $argerr + 1`
+fi
+if test -z "$token"; then
+  echo "Must specify token with -T"
+  argerr=`expr $argerr + 1`
+fi
+if test $argerr -gt 1; then
+  usage
+fi
+
+prms=
+if test -n "$cause"; then
+  prms="&cause=Cause+$cause"
+fi
+if test -n "$svnbranch"; then
+  prms="${prms}&svnbranch=$svnbranch"
+fi
+if test -n "$target"; then
+  prms="${prms}&target=$target"
+fi
+url="http://$baseurl/job/$job/buildWithParameters?token=${token}${prms}"
+
+echo "Will post to $url"
+
+cmd="curl $url"
+echo "$cmd"
+# eval "$cmd"
