fec9319c99367463e717ccedf19cc4b36c8a53bf
cary <cary@a9e16012-fd73-49ee-95cb-036de7809213>
2015-09-19 17:05:40 +0000

    Allow for multiple emails.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@2568 a9e16012-fd73-49ee-95cb-036de7809213

--- jenkins/invokejenkins.sh
+++ jenkins/invokejenkins.sh
@@ -8,27 +8,30 @@ Usage: invokebuild.sh [options]
 Options:
   -b <svnbranch>.. The branch to build
   -c <cause> ..... Jenkins puts this as the cause of the build
+  -e <email> ..... Where to email the results
   -h ............. Print this message.
   -j <job> ....... The Jenkins job, e.g., vorpal-test
   -t <target> .... The target for the build
   -T <token> ..... The job remote invocation token
   -u <baseurl> ... The base url, e.g., carys.org:8300
 EOF
-  exit
+  exit $1
 }
 
 svnbranch=
 cause=
+email=
 job=
 target=
 token=
 baseurl=
 
-while getopts "b:c:hj:t:T:u:" arg; do
+while getopts "b:c:e:hj:t:T:u:" arg; do
   case $arg in
     b) svnbranch=$OPTARG;;
     c) cause=$OPTARG;;
-    h) usage;;
+    e) email=${email},$OPTARG;;
+    h) usage 0;;
     j) job=$OPTARG;;
     T) token=$OPTARG;;
     t) target=$OPTARG;;
@@ -51,12 +54,15 @@ if test -z "$token"; then
   argerr=`expr $argerr + 1`
 fi
 if test $argerr -gt 1; then
-  usage
+  usage 1
 fi
 
 prms=
 if test -n "$cause"; then
   prms="&cause=Cause+$cause"
+fi
+if test -n "$email"; then
+  prms="${prms}&email=$email"
 fi
 if test -n "$svnbranch"; then
   prms="${prms}&svnbranch=$svnbranch"
