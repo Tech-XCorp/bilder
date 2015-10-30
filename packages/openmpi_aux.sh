187c0555dbe13f92f4862e0d9191e385ce44da95
alexanda <alexanda@a9e16012-fd73-49ee-95cb-036de7809213>
2015-10-30 23:19:29 +0000

    Reverting r2658 as valgrind is a soft dependency for openmpi in that if valgrind is built or rebuilt then openmpi should be rebuilt.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@2659 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/openmpi_aux.sh
+++ packages/openmpi_aux.sh
@@ -24,7 +24,7 @@ setOpenmpiTriggerVars() {
       *) OPENMPI_BUILDS=nodl,static,shared;;
     esac
   fi
-  OPENMPI_DEPS=libtool,automake
+  OPENMPI_DEPS=valgrind,libtool,automake
 }
 setOpenmpiTriggerVars
 
