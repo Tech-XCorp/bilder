3851a456f88ace827ec27b39bb55ca46433455c1
cary <cary@a9e16012-fd73-49ee-95cb-036de7809213>
2015-11-07 18:45:49 +0000

    Fix typo.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@2708 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/openmpi_aux.sh
+++ packages/openmpi_aux.sh
@@ -19,7 +19,7 @@ setOpenmpiTriggerVars() {
   OPENMPI_BLDRVERSION_STD=1.6.5
   OPENMPI_BLDRVERSION_EXP=1.8.6
   if $BUILD_MPIS && test -z "$OPENMPI_BUILDS" && [[ $USE_MPI =~ openmpi ]]; then
-    OPENMPI_BUILDS=nodl,static,shared;;
+    OPENMPI_BUILDS=nodl,static,shared
   fi
   OPENMPI_DEPS=valgrind,libtool,automake
 }
