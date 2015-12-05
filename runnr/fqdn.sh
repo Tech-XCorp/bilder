4fd6ffae23b952af70eef04aa6df9955cc497eab
cary <cary@a9e16012-fd73-49ee-95cb-036de7809213>
2015-12-05 09:25:29 +0000

    Generic way of getting mailhost for nersc like systems.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@2774 a9e16012-fd73-49ee-95cb-036de7809213

--- runnr/fqdn.sh
+++ runnr/fqdn.sh
@@ -42,32 +42,8 @@ bilderFqdn() {
 
 # Otherwise assume domainname is last part and strip off trailing
 # numbers to get mailhost
-  DOMAINNAME=`echo $FQMAILHOST | sed -e 's/^[^\.]*\.//'`
-  FQMAILHOST=`echo $FQMAILHOST | sed -e 's/\..*$//' -e 's/[0-9]*$//'`
-
-# Obsolete
-if false; then
-  case $fqdn in
-# ALCF
-    *.*.alcf.anl.gov)
-      FQMAILHOST=`echo $FQHOSTNAME | sed -e 's/.alcf.anl.gov//' -e 's/^.*\.//'`.alcf.anl.gov
-      DOMAINNAME=${DOMAINNAME:-"alcf.anl.gov"}
-      RUNNRSYSTEM=${RUNNRSYSTEM:-"BGP"}
-      ;;
-# NERSC (many machines do not return fqdn)
-    edison[0-9]*)
-      echo "Working on edison."
-      FQHOSTNAME=${FQHOSTNAME:-"$fqdn.nersc.gov"}
-      FQMAILHOST=${FQMAILHOST:-"edison.nersc.gov"}
-      RUNNRSYSTEM=${RUNNRSYSTEM:-"XC30"}
-      ;;
-    hopper[01][0-9]*)
-      FQHOSTNAME=${FQHOSTNAME:-"$fqdn.nersc.gov"}
-      FQMAILHOST=${FQMAILHOST:-"hopper.nersc.gov"}
-      RUNNRSYSTEM=${RUNNRSYSTEM:-"XE6"}
-      ;;
-  esac
-fi
+  DOMAINNAME=`echo $FQHOSTNAME | sed -e 's/^[^\.]*\.//'`
+  FQMAILHOST=`echo $FQHOSTNAME | sed -e 's/\..*$//' -e 's/[0-9]*$//'`.$DOMAINNAME
 
 }
 
