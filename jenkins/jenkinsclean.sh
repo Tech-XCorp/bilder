618ac694250a27d05df522027d05fc320f8d9e28
cary <cary@a9e16012-fd73-49ee-95cb-036de7809213>
2016-01-17 14:47:39 +0000

    Updating cleaning list.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@2892 a9e16012-fd73-49ee-95cb-036de7809213

--- jenkins/jenkinsclean.sh
+++ jenkins/jenkinsclean.sh
@@ -6,7 +6,7 @@
 # These artifacts are already created at clean time
 keepartifacts='jenkinsbild.log,builds*/*.log'
 # These artifacts can be cleaned
-rmartifacts='builds*/bilderenv.txt,builds*/*-summary.txt,builds*/*-chain.txt,*/*-preconfig.sh,*/preconfig.txt,builds*/*/*/*-config.sh,builds*/*/*/*-config.txt,builds*/*/*/*-build.sh,builds*/*/*/*-build.txt,builds*/*/*/*-install.sh,builds*/*/*/*-install.txt,builds*/*/*/*-test.sh,builds*/*/*/*-test.txt,builds*/*/*-config.sh,builds*/*/*-config.txt,builds*/*/*-build.sh,builds*/*/*-build.txt,builds*/*/*-install.sh,builds*/*/*-install.txt,*tests/*-config.sh,*tests/*-config.txt,*tests/*-build.sh,*tests/*-build.txt,*tests/*-install.sh,*tests/*-install.txt,*tests/runtxtest*.txt,*tests/check*.txt,*tests/check.*'
+rmartifacts='builds*/bilderenv.txt,builds*/*-summary.txt,builds*/*-abstract.txt,builds*/*-chain.txt,*/*-preconfig.sh,*/*-preconfig.txt,builds*/*/*/*-config.sh,builds*/*/*/*-config.txt,builds*/*/*/*-build.sh,builds*/*/*/*-build.txt,builds*/*/*/*-install.sh,builds*/*/*/*-install.txt,builds*/*/*/*-test.sh,builds*/*/*/*-test.txt,builds-*/*/*/Testing/Temporary/*,builds*/*/*-config.sh,builds*/*/*-config.txt,builds*/*/*-build.sh,builds*/*/*-build.txt,builds*/*/*-install.sh,builds*/*/*-install.txt,*tests/*-config.sh,*tests/*-config.txt,*tests/*-build.sh,*tests/*-build.txt,*tests/*-install.sh,*tests/*-install.txt,*tests/runtxtest*.txt,*tests/check*.txt,*tests/check.*'
 echo "Removing artifacts, $rmartifacts in $PWD."
 bashartifacts=`echo $rmartifacts | sed 's/,/ /g'`
 cmd="rm -f $bashartifacts"
