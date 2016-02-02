b8fca4205da48403e46f04dea5f2696cbcb61d33
cary <cary@a9e16012-fd73-49ee-95cb-036de7809213>
2016-02-02 21:33:58 +0000

    Differentiating numpy versions for installation args.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@2962 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/numpy.sh
+++ packages/numpy.sh
@@ -171,11 +171,13 @@ buildNumpy() {
 # installations need the following arguments.  Otherwise, the installation
 # is inside an egg, and the regular python path does not work.
 # At the moment, the below fixes windows but not darwin/linux.
-  case `uname` in
-    CYGWIN*)
-      NUMPY_INSTALL_ARGS="--single-version-externally-managed --record='$PYTHON_SITEPKGSDIR/numpy.files'"
-      ;;
-  esac
+  if [[ $NUMPY_BLDRVERSION =~ 1.1[0-9] ]]; then
+    case `uname` in
+      CYGWIN*)
+        NUMPY_INSTALL_ARGS="--single-version-externally-managed --record='$PYTHON_SITEPKGSDIR/numpy.files'"
+        ;;
+    esac
+  fi
 # For Cygwin, build, install, and make packages all at once, with
 # the latter if not building from a repo, as controlled by BDIST_WININST_ARG.
 # For others, just build.
