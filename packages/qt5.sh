079bd5d9a40d0a0b11f8bc6f790962d203e041b5
swsides <swsides@a9e16012-fd73-49ee-95cb-036de7809213>
2016-10-24 18:36:22 +0000

    Making more name changes and putting correct qt version in qt5_aux.sh file.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/branches/visit292-r946-swsides@3207 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/qt5.sh
+++ packages/qt5.sh
@@ -1,19 +1,19 @@
 # #!/bin/bash
 #
-# Build information for qt.
+# Build information for qt5.
 #
-# $Id: qt.sh 2982 2016-02-06 21:19:04Z cary $
+# $Id: qt5.sh 2982 2016-02-06 21:19:04Z cary $
 #
 ######################################################################
 
 ######################################################################
 #
-# Trigger variables set in qt_aux.sh
+# Trigger variables set in qt5_aux.sh
 #
 ######################################################################
 
 mydir=`dirname $BASH_SOURCE`
-source $mydir/qt_aux.sh
+source $mydir/qt5_aux.sh
 
 ######################################################################
 #
@@ -23,25 +23,25 @@ source $mydir/qt_aux.sh
 #
 ######################################################################
 
-setQtNonTriggerVars() {
-  QT_UMASK=002
+setQt5NonTriggerVars() {
+  QT5_UMASK=002
 }
-setQtNonTriggerVars
+setQt5NonTriggerVars
 
 ######################################################################
 #
-# Launch qt builds.
+# Launch qt5 builds.
 #
 ######################################################################
 
-buildQt() {
+buildQt5() {
 
-# Qt requires g++ to have that precise name
+# Qt5 requires g++ to have that precise name
 # If different, link it into contrib's bin dir.
-  QT_GXX_LINKED=false
+  QT5_GXX_LINKED=false
 
-# QT must be built in place
-  if ! bilderUnpack -i qt; then
+# QT5 must be built in place
+  if ! bilderUnpack -i qt5; then
     return
   fi
 
@@ -49,7 +49,7 @@ buildQt() {
   case `uname` in
     Linux | Darwin)
       if test "$(basename $PYC_CXX)" != g++ -a ! -f ${CONTRIB_DIR}/bin/g++; then
-        QT_GXX_LINKED=true
+        QT5_GXX_LINKED=true
         mkdir -p ${CONTRIB_DIR}/bin
         ln -s `which $PYC_CXX` ${CONTRIB_DIR}/bin/g++
         addtopathvar PATH $CONTRIB_DIR/bin
@@ -59,21 +59,21 @@ buildQt() {
 
 # Get the addition args and environment per-platform.
 # Get the phonon args separately to allow experimental builds without it.
-  local QT_ADDL_ARGS=
+  local QT5_ADDL_ARGS=
   local ldlibpath=
-  local QT_ENV=
-  local QT_PHONON_ARGS=-phonon
+  local QT5_ENV=
+  local QT5_PHONON_ARGS=-phonon
   case `uname` in
 
     CYGWIN*)
       ;;
 
     Darwin)
-# jpeg present, but qt cannot find headers
+# jpeg present, but qt5 cannot find headers
       if echo $CXXFLAGS | grep -q stdlib=libc++; then
-        QT_ADDL_ARGS="$QT_ADDL_ARGS -platform unsupported/macx-clang-libc++"
+        QT5_ADDL_ARGS="$QT5_ADDL_ARGS -platform unsupported/macx-clang-libc++"
       else
-        QT_ADDL_ARGS="$QT_ADDL_ARGS -platform macx-g++"
+        QT5_ADDL_ARGS="$QT5_ADDL_ARGS -platform macx-g++"
       fi
       case `uname -r` in
         13.*)
@@ -81,31 +81,31 @@ buildQt() {
           ;;
         1[0-2].*)
           case `uname -m` in
-            i386) QT_ADDL_ARGS="$QT_ADDL_ARGS -arch x86_64";;
+            i386) QT5_ADDL_ARGS="$QT5_ADDL_ARGS -arch x86_64";;
           esac
           ;;
       esac
-      case $QT_BLDRVERSION in
+      case $QT5_BLDRVERSION in
         5.*) ;;
-        *) QT_ADDL_ARGS="$QT_ADDL_ARGS -cocoa";;
+        *) QT5_ADDL_ARGS="$QT5_ADDL_ARGS -cocoa";;
       esac
-      QT_ADDL_ARGS="$QT_ADDL_ARGS -no-gif -qt-libpng"
+      QT5_ADDL_ARGS="$QT5_ADDL_ARGS -no-gif -qt5-libpng"
       ;;
 
     Linux)
-# Adding to the LD_RUN_PATH gets rpath set for the qt libs.
-# Adding to the LD_LIBRARY_PATH gets around the missing QtCLucene link bug.
+# Adding to the LD_RUN_PATH gets rpath set for the qt5 libs.
+# Adding to the LD_LIBRARY_PATH gets around the missing Qt5CLucene link bug.
 # To get around bash space separation of string, we separate env settings
 # with a comma.
       case `uname -m` in
         x86_64)
-          QT_ADDL_ARGS="$QT_ADDL_ARGS -platform linux-g++-64"
+          QT5_ADDL_ARGS="$QT5_ADDL_ARGS -platform linux-g++-64"
           ;;
         *)
-          QT_ADDL_ARGS="$QT_ADDL_ARGS -platform linux-g++"
+          QT5_ADDL_ARGS="$QT5_ADDL_ARGS -platform linux-g++"
           ;;
       esac
-      QT_ADDL_ARGS="$QT_ADDL_ARGS -system-libpng"
+      QT5_ADDL_ARGS="$QT5_ADDL_ARGS -system-libpng"
 
 # Need the following for phonon (and for webkit):
 #   glib (aka glib2 for the rpm)
@@ -127,7 +127,7 @@ buildQt() {
         fi
       done
       if test -n "${gstprefix}"; then
-        QT_PHONON_ARGS="$QT_PHONON_ARGS -L$gstprefix/lib -I$gstprefix/include/gstreamer-0.10"
+        QT5_PHONON_ARGS="$QT5_PHONON_ARGS -L$gstprefix/lib -I$gstprefix/include/gstreamer-0.10"
         gstlibdir=$gstprefix/lib
       fi
 
@@ -138,24 +138,24 @@ buildQt() {
 #  -I/usr/include/libxml2
 
       local incdir=
-# qt will not compile with gstreamer-1.0, so specifically look for 0.10
+# qt5 will not compile with gstreamer-1.0, so specifically look for 0.10
       local gpkgs="libxml2 dbus-1.0 glib-2.0"
       test -z "${gstprefix}" && gpkgs="$gpkgs gstreamer-0.10"
       for i in $gpkgs; do
 # Get the latest
         incdir=`ls -1d /usr/include/$i{,-*} 2>/dev/null | tail -1`
         if test -n "$incdir"; then
-          QT_PHONON_ARGS="$QT_PHONON_ARGS -I$incdir"
+          QT5_PHONON_ARGS="$QT5_PHONON_ARGS -I$incdir"
         else
-          techo "WARNING: [qt.sh] May need to install ${i}-devel."
+          techo "WARNING: [qt5.sh] May need to install ${i}-devel."
         fi
       done
 # glib a little special to deal with versions
       incdir=`ls -1d /usr/include/glib-* 2>/dev/null | tail -1`
       if test -n "$incdir"; then
-        QT_PHONON_ARGS="$QT_PHONON_ARGS -I$incdir"
+        QT5_PHONON_ARGS="$QT5_PHONON_ARGS -I$incdir"
       else
-        techo "WARNING: [qt.sh] May need to install glib2-devel."
+        techo "WARNING: [qt5.sh] May need to install glib2-devel."
       fi
 # On different distros, this include directory can be in different places
       local glibbn=`basename $incdir`
@@ -172,63 +172,63 @@ buildQt() {
         fi
       done
       if test -n "$glibincdir"; then
-        QT_PHONON_ARGS="$QT_PHONON_ARGS -I$glibincdir -ldbus-1 -lglib-2.0 -lgthread-2.0 -lgstreamer-0.10 -lgobject-2.0"
+        QT5_PHONON_ARGS="$QT5_PHONON_ARGS -I$glibincdir -ldbus-1 -lglib-2.0 -lgthread-2.0 -lgstreamer-0.10 -lgobject-2.0"
       else
-        techo "WARNING: [qt.sh] glib word-size include dir not found."
-        techo "WARNING: [qt.sh] May need to install glib2-devel."
+        techo "WARNING: [qt5.sh] glib word-size include dir not found."
+        techo "WARNING: [qt5.sh] May need to install glib2-devel."
       fi
       local gstprobe=`find /usr/include -name gstappsrc.h`
       if test -z "$gstprobe"; then
-        techo "WARNING: [qt.sh] May need to install gstreamer-plugins-base-devel."
+        techo "WARNING: [qt5.sh] May need to install gstreamer-plugins-base-devel."
       fi
 # Adjust for possible change to typedef in glib
       if grep -q 'union *_GMutex' $incdir/glib/gthread.h; then
-        techo "Adjusting Qt for change in gthread.h."
-        local qtgtypedefs=$BUILD_DIR/qt-$QT_BLDRVERSION/$QT_BUILD/src/3rdparty/webkit/Source/JavaScriptCore/wtf/gobject/GTypedefs.h
-        cmd="sed -i.bak 's/struct _GMutex/union _GMutex/' $qtgtypedefs"
+        techo "Adjusting Qt5 for change in gthread.h."
+        local qt5gtypedefs=$BUILD_DIR/qt5-$QT5_BLDRVERSION/$QT5_BUILD/src/3rdparty/webkit/Source/JavaScriptCore/wtf/gobject/GTypedefs.h
+        cmd="sed -i.bak 's/struct _GMutex/union _GMutex/' $qt5gtypedefs"
         techo "$cmd"
         eval "$cmd"
       fi
-      QT_ENV="LD_RUN_PATH=${CONTRIB_DIR}/mesa-mgl/lib:$LD_RUN_PATH LD_LIBRARY_PATH=$BUILD_DIR/qt-$QT_BLDRVERSION/$QT_BUILD/lib:$LD_LIBRARY_PATH"
+      QT5_ENV="LD_RUN_PATH=${CONTRIB_DIR}/mesa-mgl/lib:$LD_RUN_PATH LD_LIBRARY_PATH=$BUILD_DIR/qt5-$QT5_BLDRVERSION/$QT5_BUILD/lib:$LD_LIBRARY_PATH"
       ;;
 
   esac
 
-# PyQt will not build on Linux when Qt is built without phonon, so restoring.
+# PyQt will not build on Linux when Qt5 is built without phonon, so restoring.
 # Phonon is also required for WebKit, which the composers need.
 # On Linux, this requires glib2-devel and gstreamer-plugins-base-devel
-  QT_WITH_PHONON=${QT_WITH_PHONON:-"true"}
-  if ! $QT_WITH_PHONON; then
-    techo "NOTE: Building Qt without phonon."
-    QT_PHONON_ARGS=-no-phonon
+  QT5_WITH_PHONON=${QT5_WITH_PHONON:-"true"}
+  if ! $QT5_WITH_PHONON; then
+    techo "NOTE: Building Qt5 without phonon."
+    QT5_PHONON_ARGS=-no-phonon
   fi
 
 # Version dependent args
-# qt-5 configures differently
+# qt5-5 configures differently
 # make -j does not work with 4.8.6, apparently.
-  case $QT_BLDRVERSION in
+  case $QT5_BLDRVERSION in
     5.*)
-      QT_VERSION_ARGS="-developer-build"
+      QT5_VERSION_ARGS="-developer-build"
       ;;
     4.8.6)
-      QT_VERSION_ARGS="-buildkey bilder -no-libtiff -declarative -webkit $QT_PHONON_ARGS"
-      QT_MAKEJ_USEARGS="$QT_MAKEJ_ARGS"
+      QT5_VERSION_ARGS="-buildkey bilder -no-libtiff -declarative -webkit $QT5_PHONON_ARGS"
+      QT5_MAKEJ_USEARGS="$QT5_MAKEJ_ARGS"
       ;;
     4.*)
-      QT_VERSION_ARGS="-buildkey bilder -no-libtiff -declarative -webkit $QT_PHONON_ARGS"
-      QT_MAKEJ_USEARGS="$QT_MAKEJ_ARGS"
+      QT5_VERSION_ARGS="-buildkey bilder -no-libtiff -declarative -webkit $QT5_PHONON_ARGS"
+      QT5_MAKEJ_USEARGS="$QT5_MAKEJ_ARGS"
       ;;
   esac
 
 # Restore dbus and xmlpatterns or get wrong one
-  local otherargsvar=`genbashvar QT_${QT_BUILD}`_OTHER_ARGS
+  local otherargsvar=`genbashvar QT5_${QT5_BUILD}`_OTHER_ARGS
   local otherargsval=`deref ${otherargsvar}`
-  if bilderConfig -i qt $QT_BUILD "$QT_ADDL_ARGS $QT_VERSION_ARGS -confirm-license -make libs -make tools -fast -opensource -opengl -no-separate-debug-info -no-sql-db2 -no-sql-ibase -no-sql-mysql -no-sql-oci -no-sql-odbc -no-sql-psql -no-sql-sqlite -no-sql-sqlite2 -no-sql-tds -no-javascript-jit -nomake docs -nomake examples -nomake demos $otherargsval" "" "$QT_ENV"; then
+  if bilderConfig -i qt5 $QT5_BUILD "$QT5_ADDL_ARGS $QT5_VERSION_ARGS -confirm-license -make libs -make tools -fast -opensource -opengl -no-separate-debug-info -no-sql-db2 -no-sql-ibase -no-sql-mysql -no-sql-oci -no-sql-odbc -no-sql-psql -no-sql-sqlite -no-sql-sqlite2 -no-sql-tds -no-javascript-jit -nomake docs -nomake examples -nomake demos $otherargsval" "" "$QT5_ENV"; then
 # Make clean seems to hang
-    bilderBuild -k qt $QT_BUILD "$QT_MAKEJ_USEARGS" "$QT_ENV"
+    bilderBuild -k qt5 $QT5_BUILD "$QT5_MAKEJ_USEARGS" "$QT5_ENV"
   else
 # Remove linked file if present
-    if $QT_GXX_LINKED; then
+    if $QT5_GXX_LINKED; then
       rm -f ${CONTRIB_DIR}/bin/g++
     fi
   fi
@@ -236,37 +236,37 @@ buildQt() {
 
 ######################################################################
 #
-# Test qt
+# Test qt5
 #
 ######################################################################
 
-testQt() {
-  techo "Not testing qt."
+testQt5() {
+  techo "Not testing qt5."
 }
 
 ######################################################################
 #
-# Install qt
+# Install qt5
 #
 ######################################################################
 
 #
-# Fix up various problems (bad links) with QT installations
-# We used to change the installation names of qt in VisIt, but now
+# Fix up various problems (bad links) with QT5 installations
+# We used to change the installation names of qt5 in VisIt, but now
 # VisIt takes care of that in its build system.
 #
-fixQtInstall() {
+fixQt5Install() {
 # Remove linked file if present
-  if $QT_GXX_LINKED; then
+  if $QT5_GXX_LINKED; then
     rm -f ${CONTRIB_DIR}/bin/g++
   fi
-# Fix the bad node that Qt leaves behind on a Darwin installation.
+# Fix the bad node that Qt5 leaves behind on a Darwin installation.
 # (Seems not to do this on an overinstallation?)
   case `uname` in
     Darwin)
-      local badnode="$CONTRIB_DIR/qt-$QT_BLDRVERSION-$QT_BUILD/lib/QtTest.framework/Versions/4/4"
+      local badnode="$CONTRIB_DIR/qt5-$QT5_BLDRVERSION-$QT5_BUILD/lib/Qt5Test.framework/Versions/4/4"
       if test -L $badnode; then
-        techo "NOTE: Removing leftover link from qt installation, $badnode."
+        techo "NOTE: Removing leftover link from qt5 installation, $badnode."
         cmd="chmod -h u+rwx $badnode"
         techo "$cmd"
         $cmd
@@ -275,35 +275,35 @@ fixQtInstall() {
         $cmd
       fi
       if test -L $badnode; then
-        techo "WARNING: Unable to remove leftover link, $badnode, from qt installation.  Must be done as root."
+        techo "WARNING: Unable to remove leftover link, $badnode, from qt5 installation.  Must be done as root."
       fi
       ;;
   esac
 }
 
 #
-# Install Qt
+# Install Qt5
 #
-installQt() {
-  local qt_tried=false
-  local qtpid=`deref QT_${QT_BUILD}_PID`
-  if test -n "$qtpid"; then
-    qt_tried=true
+installQt5() {
+  local qt5_tried=false
+  local qt5pid=`deref QT5_${QT5_BUILD}_PID`
+  if test -n "$qt5pid"; then
+    qt5_tried=true
   fi
-  if bilderInstall -r -p open qt $QT_BUILD; then
-    fixQtInstall
-  elif $qt_tried; then
+  if bilderInstall -r -p open qt5 $QT5_BUILD; then
+    fixQt5Install
+  elif $qt5_tried; then
     cat <<EOF | tee -a $LOGFILE
-Qt failed to build.  Bilder will try the following:
-  cd $BUILD_DIR/qt-$QT_BLDRVERSION/$QT_BUILD
+Qt5 failed to build.  Bilder will try the following:
+  cd $BUILD_DIR/qt5-$QT5_BLDRVERSION/$QT5_BUILD
   make -i install
 and then follow with the usual installation.
 EOF
-    cd $BUILD_DIR/qt-$QT_BLDRVERSION/$QT_BUILD
-    make -i install 2>&1 | tee qt-install2.txt
-    bilderBuild qt $QT_BUILD "$QT_MAKEJ_ARGS"
-    if bilderInstall -r -p open qt $QT_BUILD; then
-      fixQtInstall
+    cd $BUILD_DIR/qt5-$QT5_BLDRVERSION/$QT5_BUILD
+    make -i install 2>&1 | tee qt5-install2.txt
+    bilderBuild qt5 $QT5_BUILD "$QT5_MAKEJ_ARGS"
+    if bilderInstall -r -p open qt5 $QT5_BUILD; then
+      fixQt5Install
     else
       techo "Extra build steps failed."
     fi
--- packages/qt5_aux.sh
+++ packages/qt5_aux.sh
@@ -2,6 +2,9 @@
 #
 # Trigger vars and find information
 #
+# NOTE: As of 10/21/2016 these instructions need updating once
+# the package details are settled.
+#
 # Latest source packages available from
 #   http://get.qt.nokia.com/qt/source/.
 #   20121125: Moved to http://qt-project.org/downloads.
@@ -30,13 +33,13 @@
 #
 ######################################################################
 
-setQtTriggerVars() {
-  QT_BLDRVERSION_STD=${QT_BLDRVERSION_STD:-"5.6.1"}
-  #  QT_BLDRVERSION_EXP=${QT_BLDRVERSION_EXP:-"4.8.7"}
-  QT_BUILDS=${QT_BUILDS:-"$FORPYTHON_SHARED_BUILD"}
-  QT_BUILD=$FORPYTHON_SHARED_BUILD
-  # QT_DEPS=gst-plugins-base,bzip2
-  QT_DEPS=bzip2
+setQt5TriggerVars() {
+  QT5_BLDRVERSION_STD=${QT5_BLDRVERSION_STD:-"5.6.1"}
+  #  QT5_BLDRVERSION_EXP=${QT5_BLDRVERSION_EXP:-"4.8.7"}
+  QT5_BUILDS=${QT5_BUILDS:-"$FORPYTHON_SHARED_BUILD"}
+  QT5_BUILD=$FORPYTHON_SHARED_BUILD
+  # QT5_DEPS=gst-plugins-base,bzip2
+  QT5_DEPS=bzip2
 # Need the following for phonon (and for webkit) on Linux:
 #   dbus
 #   glib (aka glib2 for the rpm)
@@ -44,7 +47,7 @@ setQtTriggerVars() {
 #   gstreamer-plugins-base-devel
 #   libxml2
 }
-setQtTriggerVars
+setQt5TriggerVars
 
 ######################################################################
 #
@@ -53,11 +56,11 @@ setQtTriggerVars
 ######################################################################
 
 #
-# print Qt vars
+# print Qt5 vars
 #
-printQtVars() {
-  local qtvars="QMAKE QTDIR QT_BINDIR"
-  for i in $qtvars; do
+printQt5Vars() {
+  local qt5vars="QMAKE QT5DIR QT5_BINDIR"
+  for i in $qt5vars; do
     printvar $i
   done
 }
@@ -96,62 +99,63 @@ setQmakeArgs() {
 
 }
 
-findQt() {
+findQt5() {
 
 # Try accepting what the user specified
-  if test -n "$QT_BINDIR"; then
-    if QMAKE=`env PATH=$QT_BINDIR:/usr/bin which qmake 2>/dev/null`; then
-      techo "qmake found in $QT_BINDIR"
-      addtopathvar PATH $QT_BINDIR
-      QTDIR=`dirname $QT_BINDIR`
+  if test -n "$QT5_BINDIR"; then
+    if QMAKE=`env PATH=$QT5_BINDIR:/usr/bin which qmake 2>/dev/null`; then
+      techo "qmake found in $QT5_BINDIR"
+      addtopathvar PATH $QT5_BINDIR
+      QT5DIR=`dirname $QT5_BINDIR`
       setQmakeArgs
-      printQtVars
+      printQt5Vars
       return
     else
-      techo "Qt not found in $QT_BINDIR."
-      unset QT_BINDIR
-      unset QTDIR
+      techo "Qt5 not found in $QT5_BINDIR."
+      unset QT5_BINDIR
+      unset QT5DIR
     fi
   else
-    techo "QT_BINDIR not set."
+    techo "QT5_BINDIR not set."
   fi
 
-# Seek Qt in the contrib directory
+# Seek Qt5 in the contrib directory
   local libname=
   if [[ `uname` =~ CYGWIN ]]; then
-    libname=QtCore4
+lsikdjf    libname=QtCore4 
   else
-    libname=QtCore
+sdldikjf    libname=QtCore
   fi
-  findContribPackage Qt $libname sersh pycsh
-  techo "QT_SERSH_DIR = $QT_SERSH_DIR."
-  findPycshDir Qt
-  if test -n "$QT_PYCSH_DIR"; then
-    techo "QT_PYCSH_DIR = $QT_PYCSH_DIR."
+  
+  findContribPackage Qt5 $libname sersh pycsh
+  techo "QT5_SERSH_DIR = $QT5_SERSH_DIR."
+  findPycshDir Qt5
+  if test -n "$QT5_PYCSH_DIR"; then
+    techo "QT5_PYCSH_DIR = $QT5_PYCSH_DIR."
   else
-    findContribPackage Qt $libname ser
+    findContribPackage Qt5 $libname ser
   fi
-  local qtdir=$QT_PYCSH_DIR
-  qtdir=${qtdir:-"$QT_SER_DIR"}
-  if test -n "$qtdir"; then
-    techo "Qt found in $CONTRIB_DIR."
-    QT_BINDIR=$qtdir/bin
-    addtopathvar PATH $QT_BINDIR
-    QMAKE=`env PATH=$QT_BINDIR:/usr/bin which qmake 2>/dev/null`
-    QTDIR=$qtdir
+  local qt5dir=$QT5_PYCSH_DIR
+  qt5dir=${qt5dir:-"$QT5_SER_DIR"}
+  if test -n "$qt5dir"; then
+    techo "Qt5 found in $CONTRIB_DIR."
+    QT5_BINDIR=$qt5dir/bin
+    addtopathvar PATH $QT5_BINDIR
+    QMAKE=`env PATH=$QT5_BINDIR:/usr/bin which qmake 2>/dev/null`
+    QT5DIR=$qt5dir
     setQmakeArgs
-    printQtVars
+    printQt5Vars
     return
   fi
-  techo "Qt not found in $CONTRIB_DIR."
+  techo "Qt5 not found in $CONTRIB_DIR."
 
 # Seek qmake in one's path
   if QMAKE=`which qmake 2>/dev/null`; then
-    QT_BINDIR=`dirname $QMAKE`
-    addtopathvar PATH $QT_BINDIR
-    QTDIR=`dirname $QT_BINDIR`
+    QT5_BINDIR=`dirname $QMAKE`
+    addtopathvar PATH $QT5_BINDIR
+    QT5DIR=`dirname $QT5_BINDIR`
     setQmakeArgs
-    printQtVars
+    printQt5Vars
     return
   fi
   techo "qmake not found in the user path = $PATH."
@@ -159,21 +163,21 @@ findQt() {
 # Look in standard windows installation dirs
   if [[ `uname` =~ CYGWIN ]]; then
     techo "Searching Windows installation directories."
-    QT_BVER=`ls /cygdrive/c/Qt | grep "^4" | tail -1`
-    if test -n "$QT_BVER"; then
-      QT_BINDIR=/cygdrive/c/Qt/$QT_BVER/bin
-      addtopathvar PATH $QT_BINDIR
-      QMAKE=`env PATH=$QT_BINDIR which qmake 2>/dev/null`
-      QTDIR=`dirname $QT_BINDIR`
+    QT5_BVER=`ls /cygdrive/c/Qt5 | grep "^4" | tail -1`
+    if test -n "$QT5_BVER"; then
+      QT5_BINDIR=/cygdrive/c/Qt5/$QT5_BVER/bin
+      addtopathvar PATH $QT5_BINDIR
+      QMAKE=`env PATH=$QT5_BINDIR which qmake 2>/dev/null`
+      QT5DIR=`dirname $QT5_BINDIR`
       setQmakeArgs
-      printQtVars
+      printQt5Vars
       return
     fi
-    techo "Qt not found in the Windows standard installation areas."
+    techo "Qt5 not found in the Windows standard installation areas."
   fi
 
 # Out of ideas
-  techo "findQt could not find Qt."
+  techo "findQt5 could not find Qt5."
   return 0
 }
 
