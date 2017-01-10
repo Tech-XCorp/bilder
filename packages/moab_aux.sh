fc32cb743f099ebaa3b98737a1ab828ff64766ae
beckwith <beckwith@a9e16012-fd73-49ee-95cb-036de7809213>
2017-01-10 21:50:29 +0000

    Switch stable branch; previous stable branch was corrupted.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@3391 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/moab_aux.sh
+++ packages/moab_aux.sh
@@ -17,7 +17,7 @@
 
 setMoabTriggerVars() {
   MOAB_REPO_URL=https://bitbucket.org/cadg4/moab.git
-  MOAB_REPO_BRANCH_STD=20170106-stable
+  MOAB_REPO_BRANCH_STD=stable-20170106
   MOAB_REPO_BRANCH_EXP=master
   MOAB_UPSTREAM_URL=https://bitbucket.org/fathomteam/moab.git
   MOAB_UPSTREAM_BRANCH_STD=master
