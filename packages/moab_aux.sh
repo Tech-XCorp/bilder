53ea9562694884a3a1782f8d1892c4d849c920c4
beckwith <beckwith@a9e16012-fd73-49ee-95cb-036de7809213>
2017-01-10 16:58:35 +0000

    Update stable,experimental branches.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@3390 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/moab_aux.sh
+++ packages/moab_aux.sh
@@ -17,8 +17,8 @@
 
 setMoabTriggerVars() {
   MOAB_REPO_URL=https://bitbucket.org/cadg4/moab.git
-  MOAB_REPO_BRANCH_STD=stable-puJan17
-  MOAB_REPO_BRANCH_EXP=stable-puJan17
+  MOAB_REPO_BRANCH_STD=20170106-stable
+  MOAB_REPO_BRANCH_EXP=master
   MOAB_UPSTREAM_URL=https://bitbucket.org/fathomteam/moab.git
   MOAB_UPSTREAM_BRANCH_STD=master
   MOAB_UPSTREAM_BRANCH_EXP=master
