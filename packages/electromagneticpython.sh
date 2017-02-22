38b83a44a86763dc6468c6f81ed638a547d3c838
cary <cary@a9e16012-fd73-49ee-95cb-036de7809213>
2017-02-22 13:53:47 +0000

    The tarball is ElectroMagneticPython, but the package is EMpy.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@3495 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/electromagneticpython.sh
+++ packages/electromagneticpython.sh
@@ -43,28 +43,8 @@ buildElectroMagneticPython() {
     return
   fi
 
-# Remove all old installations
-  cmd="rmall ${PYTHON_SITEPKGSDIR}/ElectroMagneticPython*"
-  techo "$cmd"
-  $cmd
   ELECTROMAGNETICPYTHON_INSTALL_ARGS="--single-version-externally-managed --record='$PYTHON_SITEPKGSDIR/ElectroMagneticPython.files'"
-  case `uname`-"$CC" in
-    CYGWIN*-*cl*)
-      ELECTROMAGNETICPYTHON_ARGS="--compiler=msvc install --prefix='$NATIVE_CONTRIB_DIR' $ELECTROMAGNETICPYTHON_INSTALL_ARGS $BDIST_WININST_ARG"
-      ELECTROMAGNETICPYTHON_ENV_USED="$DISTUTILS_ENV"
-      ;;
-    CYGWIN*-mingw*)
-# Have to install with build to get both prefix and compiler correct.
-      ELECTROMAGNETICPYTHON_ARGS="--compiler=mingw32 install --prefix='$NATIVE_CONTRIB_DIR' $ELECTROMAGNETICPYTHON_INSTALL_ARGS $BDIST_WININST_ARG"
-      local mingwgcc=`which mingw32-gcc`
-      local mingwdir=`dirname $mingwgcc`
-      ELECTROMAGNETICPYTHON_ENV_USED="PATH=$mingwdir:'$PATH'"
-      ;;
-    *)
-      ELECTROMAGNETICPYTHON_ENV_USED="$DISTUTILS_ENV"
-      ;;
-  esac
-  bilderDuBuild ElectroMagneticPython "$ELECTROMAGNETICPYTHON_ARGS" "$ELECTROMAGNETICPYTHON_ENV_USED"
+  bilderDuBuild -p EMpy ElectroMagneticPython "$ELECTROMAGNETICPYTHON_ARGS"
 
 }
 
@@ -85,18 +65,6 @@ testElectroMagneticPython() {
 ######################################################################
 
 installElectroMagneticPython() {
-  local res=1
-  case `uname` in
-    CYGWIN*)
-# bilderDuInstall should not run python setup.py install, as this
-# will rebuild with cl
-      bilderDuInstall -n ElectroMagneticPython "$ELECTROMAGNETICPYTHON_ARGS" "$ELECTROMAGNETICPYTHON_ENV_USED"
-      res=$?
-      ;;
-    *)
-      bilderDuInstall ElectroMagneticPython "$ELECTROMAGNETICPYTHON_INSTALL_ARGS" "$ELECTROMAGNETICPYTHON_ENV_USED"
-      res=$?
-      ;;
-  esac
+  bilderDuInstall ElectroMagneticPython "$ELECTROMAGNETICPYTHON_INSTALL_ARGS"
 }
 
