69895d750d9abe13bdc15889b42675ca3379a17a
cary <cary@a9e16012-fd73-49ee-95cb-036de7809213>
2017-02-25 23:27:37 +0000

    Errant removal of machinefile for cygwin.vs12.  Updating args to cmake configure of python.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@3506 a9e16012-fd73-49ee-95cb-036de7809213

--- defaultsfcns.sh
+++ defaultsfcns.sh
@@ -24,7 +24,7 @@ setBilderOsVars() {
       CONTRIB_ROOTDIR=${CONTRIB_ROOTDIR:-"/winsame"}
       INSTALL_ROOTDIR=${INSTALL_ROOTDIR:-"/winsame"}
       USERINST_ROOTDIR=${USERINST_ROOTDIR:-"/winsame/$USER"}
-      # MACHINEFILE=${MACHINEFILE:-"cygwin.vs12"} # In mkall-default.sh
+      MACHINEFILE=${MACHINEFILE:-"cygwin.vs12"} # In mkall-default.sh
       if test -z "$INSTALL_SUBDIR_SFX"; then
         machsfx=`echo $MACHINEFILE | sed -e 's/^.*\.//'`
         test -n "$machsfx" && INSTALL_SUBDIR_SFX=-$machsfx
