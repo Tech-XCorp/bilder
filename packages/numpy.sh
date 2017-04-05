8e3a8ed563dc34f51eb1399e5341f57905a83ef1
cary <cary@a9e16012-fd73-49ee-95cb-036de7809213>
2017-04-05 18:44:54 +0000

    Decruftification of  numpy and scipy package files.  Committing to test on osx.
    git-svn-id: https://ice.txcorp.com/svnrepos/code/bilder/trunk@3592 a9e16012-fd73-49ee-95cb-036de7809213

--- packages/numpy.sh
+++ packages/numpy.sh
@@ -32,8 +32,6 @@ source $mydir/numpy_aux.sh
 setNumpyNonTriggerVars() {
 
 # Without fortran, numpy builds, but scipy will not.
-# There is a connection between the numpy and scipy builds,
-# with scipy using part of the numpy distutils.
 #
 # Binary builds are at
 #  https://bitbucket.org/carlkl/mingw-w64-for-python/downloads
@@ -73,7 +71,32 @@ setNumpyNonTriggerVars
 #
 ######################################################################
 
-setupNumpyBuild() {
+#
+# This sets the variables needed to build numpy, which for now
+# is done on only Linux.  Windows builds are nearly impossible
+# so for now we download them from carlkl's website.  MacOS
+# builds are easy because one simply uses -framework Accelerate,
+# which is found automatically.
+#
+# For Linux, it seems that now numpy and scipy builds work well
+# only when BLAS and LAPACK environment variables are set.  site.cfg
+# is then irrelevant.
+#
+setNumpyBuildVars() {
+
+# Darwin defines PYC_MODFLAGS = "-undefined dynamic_lookup",
+#   but not PYC_LDSHARED
+# Linux defines PYC_MODFLAGS = "-shared", but not PYC_LDSHARED
+  local linkflags="$PYCSH_ADDL_LDFLAGS $PYC_LDSHARED $PYC_MODFLAGS"
+
+# With the new setuptools, package managers that want to manage the
+# installations need the following arguments.  Otherwise, the installation
+# is inside an egg, and the regular python path does not work.
+# Doing this on CYGWIN and Darwin.
+# Ivy build indicates need to do this on Linux
+  if [[ $NUMPY_BLDRVERSION =~ 1.1[0-9] ]]; then
+    NUMPY_INSTALL_ARGS="--single-version-externally-managed --record='$PYTHON_SITEPKGSDIR/numpy.files'"
+  fi
 
 # Set the blas and lapack names.
   local blslpcklibdir=
@@ -84,30 +107,20 @@ setupNumpyBuild() {
 
   case `uname`-"$CC" in
 
-    CYGWIN*-*cl | CYGWIN*-*cl.exe)
-      lapacknames=lapack
-# Add /NODEFAULTLIB:LIBCMT to get this on the link line.
-# Worked with 1.6.x, but may not be working with 1.8.X
-# LDFLAGS did not work.  Nor did -Xlinker.
-      if $NUMPY_WIN_USE_FORTRAN && test -n "$CONTRIB_LAPACK_SERMD_DIR"; then
-        blslpckdir="$CONTRIB_LAPACK_SERMD_DIR"
-        blasnames=blas
-      else
-        blslpckdir="$CLAPACK_CMAKE_SERMD_DIR"
-        blasnames=blas,f2c
-      fi
-      blslpcklibdir=`cygpath -aw $blslpckdir | sed 's/\\\\/\\\\\\\\/g'`\\\\lib
-      blslpckincdir=`cygpath -aw $blslpckdir | sed 's/\\\\/\\\\\\\\/g'`\\\\include
-      blslpckdir=`cygpath -aw $blslpckdir | sed 's/\\\\/\\\\\\\\/g'`\\\\
-      local flibdir=
-      if test -n "$PYC_FC"; then
-        flibdir=`$PYC_FC -print-file-name=libgfortran.a`
-        flibdir=`dirname $flibdir`
-        flibdir=`cygpath -aw "$flibdir" | sed 's/\\\\/\\\\\\\\/g'`
-      fi
+# For non-Cygwin builds, the build stage does not install.
+    Darwin-*)
+# linkflags="$linkflags -bundle -Wall"
+      linkflags="$linkflags -Wall"
+      NUMPY_ENV="$DISTUTILS_ENV2 CFLAGS='-arch i386 -arch x86_64' FFLAGS='-m32 -m64'"
       ;;
 
     Linux-*)
+      linkflags="$linkflags -Wl,-rpath,${PYTHON_LIBDIR}"
+# This is a configuration parameter, not a path to the compiler.
+      NUMPY_BUILD_ARGS="--fcompiler=`basename ${PYC_FC}`"
+      local fcpath=`dirname ${PYC_FC}`
+      NUMPY_ENV="$DISTUTILS_ENV2 PATH=${PATH}:${fcpath}"
+# Get basic lapack locations
       lapacknames=`echo $LAPACK_PYCSH_LIBRARY_NAMES | sed 's/ /, /g'`
       blasnames=`echo $BLAS_PYCSH_LIBRARY_NAMES | sed 's/ /, /g'`
       if test -d "$LAPACK_PYCSH_DIR"/lib64; then
@@ -128,106 +141,16 @@ setupNumpyBuild() {
       else
         LAPACKVAL="$blslpcklibdir"
       fi
-      ;;
-
-  esac
-
-# Create site.cfg
-# Not needed on Linux
-# If lapack libs are defined, even clapack, numpy will search for
-# a fortran and use it.  If it is going to find cygwin's fortran,
-# prevent this by not defining the blas and lapack libraries.
-  local sep=','
-  if [[ `uname` =~ CYGWIN ]]; then
-    sep=';'
-  if test -n "$lapacknames" && $HAVE_SER_FORTRAN; then
-# In spite of documentation, need semicolon, not comma.
-    if test -f site.cfg.example; then
-# On Crays this is ignored, and one needs to set LAPACK and BLAS
-      techo "Creating site.cfg."
-# Format changed with 1.8.0.  All lines begin with '#', and lapack_libs
-# and blas_libs no longer specified.  They come from the section by default?
-      sed -e "s/^#\[ALL/\[ALL/" -e "s/^#\[DEFAULT/\[DEFAULT/" -e "s?^#include_dirs = /usr/local/include?include_dirs = $blslpckincdir?" -e "s?^#library_dirs = /usr/local/lib?library_dirs = ${blslpcklibdir}${sep}$flibdir?" <site.cfg.example >numpy/distutils/site.cfg
-# As of 1.11, the line beginning with #libraries disappeared.
-      # sed -i.bak -e "s?^#libraries = lapack,blas?libraries = $lapacknames,$blasnames?" numpy/distutils/site.cfg
-      sed -i.bak -e "/^include_dirs =/a\
-libraries = $lapacknames,$blasnames" numpy/distutils/site.cfg
-    else
-      techo "WARNING: [$FUNCNAME] site.cfg.example not found in $PWD.  Will not create numpy/distutils/site.cfg."
-    fi
-  else
-    techo "lapacknames or serial fortran not defined.  Will not create numpy/distutils/site.cfg."
-  fi
-  fi
-
-# Darwin defines PYC_MODFLAGS = "-undefined dynamic_lookup",
-#   but not PYC_LDSHARED
-# Linux defines PYC_MODFLAGS = "-shared", but not PYC_LDSHARED
-  local linkflags="$PYCSH_ADDL_LDFLAGS $PYC_LDSHARED $PYC_MODFLAGS"
-
-# With the new setuptools, package managers that want to manage the
-# installations need the following arguments.  Otherwise, the installation
-# is inside an egg, and the regular python path does not work.
-# Doing this on CYGWIN and Darwin.
-# Ivy build indicates need to do this on Linux
-  if [[ $NUMPY_BLDRVERSION =~ 1.1[0-9] ]]; then
-    NUMPY_INSTALL_ARGS="--single-version-externally-managed --record='$PYTHON_SITEPKGSDIR/numpy.files'"
-  fi
-
-# For Cygwin, build, install, and make packages all at once, with
-# the latter if not building from a repo, as controlled by BDIST_WININST_ARG.
-# For others, just build.
-  case `uname`-"$CC" in
-# For Cygwin builds, one has to specify the compiler during installation,
-# but then one has to be building, otherwise specifying the compiler is
-# an error.  So the only choice seems to be to install simultaneously
-# with building.  Unfortunately, one cannot then intervene between the
-# build and installation steps to remove old installations only if the
-# build was successful.  Instead one must do any removal before starting
-# the build and installation.
-    CYGWIN*-*cl*)
-      NUMPY_ARGS="--compiler=$NUMPY_WIN_CC_TYPE install --prefix='$NATIVE_CONTRIB_DIR' $NUMPY_INSTALL_ARGS $BDIST_WININST_ARG"
-      NUMPY_ENV="$DISTUTILS_ENV"
-      if $NUMPY_WIN_USE_FORTRAN && test -n "$PYC_FC"; then
-        local fcbase=`basename "$PYC_FC"`
-        if which $fcbase 1>/dev/null 2>&1; then
-# NUMPY_ARGS="--fcompiler='$fcbase' $NUMPY_ARGS"
-# The above specification fails with
-# don't know how to compile Fortran code on platform 'nt' with 'x86_64-w64-mingw32-gfortran.exe' compiler. Supported compilers are: pathf95,intelvem,absoft,compaq,ibm,sun,lahey,pg,hpux,intele,gnu95,intelv,g95,intel,compaqv,mips,vast,nag,none,intelem,gnu,intelev)
-          NUMPY_ARGS="--fcompiler=gnu95 $NUMPY_ARGS"
-          NUMPY_ENV="$NUMPY_ENV F90='$fcbase'"
-# Below does not help.  NumPy always uses 'gcc', so one must separate by path.
-# local ccbase=`echo $fcbase | sed 's/fortran/cc/g'`
-# NUMPY_ENV="$NUMPY_ENV CC='$ccbase'"
-        else
-          techo "WARNING: [$FUNCNAME] Not using fortran.  $fcbase not in path."
-        fi
-      else
-        techo "WARNING: [$FUNCNAME] Not using fortran.  PYC_FC = $PYC_FC.  NUMPY_WIN_USE_FORTRAN = $NUMPY_WIN_USE_FORTRAN."
-      fi
-    ;;
-
-# For non-Cygwin builds, the build stage does not install.
-    Darwin-*)
-# linkflags="$linkflags -bundle -Wall"
-      linkflags="$linkflags -Wall"
-      NUMPY_ENV="$DISTUTILS_ENV2 CFLAGS='-arch i386 -arch x86_64' FFLAGS='-m32 -m64'"
-      ;;
-    Linux-*)
-      linkflags="$linkflags -Wl,-rpath,${PYTHON_LIBDIR}"
-# Handle the case where PYC_FC may not be in path
-      NUMPY_ARGS="--fcompiler=`basename ${PYC_FC}`"
-      local fcpath=`dirname ${PYC_FC}`
-      NUMPY_ENV="$DISTUTILS_ENV2 PATH=${PATH}:${fcpath}"
-# With these environment variables set, rpath, library dirs, ... all found
       if test -n "$blslpcklibdir"; then
         NUMPY_ENV="$DISTUTILS_ENV2 LAPACK='$LAPACKVAL' BLAS='$BLASVAL'"
       fi
       ;;
+
     *)
       techo "WARNING: [numpy.sh] uname `uname` not recognized.  Not building."
       return
       ;;
+
   esac
   trimvar linkflags ' '
   techo "linkflags = $linkflags."
@@ -252,17 +175,18 @@ buildNumpy() {
   local cmd=
 
 # Move to build directory
-  cd $BUILD_DIR/numpy-${NUMPY_BLDRVERSION} # Needed for setupNumpyBuild or pip
+  cd $BUILD_DIR/numpy-${NUMPY_BLDRVERSION} # Needed for setNumpyBuildVars or pip
 # Build/install
   if $BLDR_BUILD_NUMPY; then
-    setupNumpyBuild
+    setNumpyBuildVars
 # For CYGWIN builds, remove any detritus lying around now.
     if [[ `uname` =~ CYGWIN ]]; then
+      techo "WARNING: [FUNCNAME] Building on cygwin not now supported."
       cmd="rmall ${PYTHON_SITEPKGSDIR}/numpy*"
       techo "$cmd"
       $cmd
     fi
-    bilderDuBuild numpy "$NUMPY_ARGS" "$NUMPY_ENV"
+    bilderDuBuild numpy "$NUMPY_BUILD_ARGS" "$NUMPY_ENV"
   else
 # We know we should build this, as there is only one build.
     cmd="python -m pip install --upgrade --target=$MIXED_PYTHON_SITEPKGSDIR -i https://pypi.binstar.org/carlkl/simple numpy"
--- packages/scipy.sh
+++ packages/scipy.sh
@@ -40,7 +40,7 @@ setScipyNonTriggerVars
 #
 ######################################################################
 
-setupScipyBuild() {
+setScipyBuildVars() {
 
 # Get native fortran compiler
   local SCIPY_FC="$PYC_FC"
@@ -48,91 +48,11 @@ setupScipyBuild() {
     SCIPY_FC=`cygpath -u "$SCIPY_FC"`
   fi
 
-# Set the blas and lapack names.
-  local blslpcklibdir=
-  local blslpckincdir=
-  local blslpckdir=
-
-# Accumulate linkflags for modules
-  local linkflags="$PYCSH_ADDL_LDFLAGS $PYC_LDSHARED $PYC_MODFLAGS"
-  SCIPY_INSTALL_ARGS="--single-version-externally-managed --record='$PYTHON_SITEPKGSDIR/scipy.files'"
-
-# Get env and args
-  case `uname`-"$CC" in
-
-    CYGWIN*-*cl*)
-# Determine basic args
-      SCIPY_BUILD_ARGS="--compiler=$NUMPY_WIN_CC_TYPE install --prefix='$NATIVE_CONTRIB_DIR' $BDIST_WININST_ARG"
-      SCIPY_BUILD_ARGS="--fcompiler=gnu95 $SCIPY_BUILD_ARGS"
-      local blslpcklibdir="$CONTRIB_LAPACK_SERMD_DIR"/lib
-      blslpcklibdir=`cygpath -aw $blslpcklibdir | sed 's/\\\\/\\\\\\\\/g'`
-      SCIPY_ENV="$DISTUTILS_ENV LAPACK='${blslpcklibdir}' BLAS='${blslpcklibdir}'"
-      local fcbase=`basename "$PYC_FC"`
-      if ! eval "$SCIPY_ENV" which $fcbase 1>/dev/null 2>&1; then
-        techo "ERROR: [$FUNCNAME] Cannot build scipy, as $fcbase is not in PATH."
-        return
-      fi
-      ;;
-
-# For non-Cygwin builds, the build stage does not install.
-    Darwin-*)
-# -bundle found necessary for scipy, otherwise
-# python -c "import scipy.interpolate"
-# ...
-# ImportError: dlopen(/contrib/lib/python2.7/site-packages/scipy/special/_ufuncs.so, 2): Symbol not found: _main
-# -bundle prevents undefined symbol, _main
-      linkflags="$linkflags -bundle -Wall"
-# See http://trac.macports.org/changeset/118776 and
-# http://trac.macports.org/browser/trunk/dports/python/py-scipy/Portfile
-      local fflags="-m32 -m64 -fno-second-underscore"
-      if [[ $PYC_CC =~ clang ]]; then
-        fflags="$fflags -ff2c"
-      fi
-# Looks like a little belt and suspenders on the options and the env
-      SCIPY_ENV="$DISTUTILS_ENV2 CFLAGS='-arch i386 -arch x86_64' FFLAGS='$fflags'"
-      SCIPY_CONFIG_ARGS="config_fc --fcompiler gnu95 --f77exec='$PYC_FC' --f77flags='$fflags' --f90exec='$PYC_FC' --f90flags='$fflags' config --cc='$PYC_CC'"
-      SCIPY_BUILD_ARGS="install --prefix='$NATIVE_CONTRIB_DIR'"
-      ;;
-
-    Linux-*)
-      SCIPY_ENV="$DISTUTILS_ENV2 $SCIPY_GFORTRAN"
-# This is the only way to specify different libraries:
-# https://www.scipy.org/scipylib/building/linux.html
-      lapacknames=`echo $LAPACK_PYCSH_LIBRARY_NAMES | sed 's/ /, /g'`
-      blasnames=`echo $BLAS_PYCSH_LIBRARY_NAMES | sed 's/ /, /g'`
-      if test -d ${LAPACK_PYCSH_DIR}/lib64; then
-        blslpcklibdir=${LAPACK_PYCSH_DIR}/lib64
-      elif test -d ${LAPACK_PYCSH_DIR}/lib; then
-        blslpcklibdir=${LAPACK_PYCSH_DIR}/lib
-      fi
-# If the name is not lapack or blas, then BLAS and LAPACK should be the
-# full path to the library.  Seen on crays.  Assumes a single name.
-      if test $blasnames != blas; then
-        BLASVAL="$blslpcklibdir"/lib${blasnames}.so
-      else
-        BLASVAL="$blslpcklibdir"
-      fi
-      if test $lapacknames != lapack; then
-        LAPACKVAL="$blslpcklibdir"/lib${lapacknames}.so
-      else
-        LAPACKVAL="$blslpcklibdir"
-      fi
-      if test -n "$blslpcklibdir"; then
-        SCIPY_ENV="$DISTUTILS_ENV2 LAPACK='$LAPACKVAL' BLAS='$BLASVAL'"
-      fi
-      linkflags="$linkflags -Wl,-rpath,${PYTHON_LIBDIR}"
-      ;;
-
-    *)
-      techo "WARNING: [$FUNCNAME] uname `uname` not recognized.  Not building."
-      return
-      ;;
-
-  esac
-  trimvar linkflags ' '
-  if test -n "$linkflags"; then
-    SCIPY_ENV="$SCIPY_ENV LDFLAGS='$linkflags'"
-  fi
+# Get build vars from numpy
+  test -z "$NUMPY_ENV" && setNumpyBuildVars
+  SCIPY_ENV="$NUMPY_ENV"
+  SCIPY_BUILD_ARGS="$NUMPY_BUILD_ARGS"
+  SCIPY_INSTALL_ARGS="$NUMPY_INSTALL_ARGS"
 
 }
 
@@ -143,19 +63,20 @@ buildScipy() {
     return
   fi
   local cmd=
-  if [[ `uname` =~ CYGWIN ]]; then
-    cmd="rmall ${PYTHON_SITEPKGSDIR}/scipy*"
-    techo "$cmd"
-    $cmd
-  fi
 
-# On hopper, cannot include LD_LIBRARY_PATH
+# Move to build directory
+  cd $BUILD_DIR/scipy-${SCIPY_BLDRVERSION}
+# Build/install
   if $BLDR_BUILD_NUMPY && $HAVE_SER_FORTRAN; then
-    setupScipyBuild
+    setScipyBuildVars
+    if [[ `uname` =~ CYGWIN ]]; then
+      cmd="rmall ${PYTHON_SITEPKGSDIR}/scipy*"
+      techo "$cmd"
+      $cmd
+    fi
     bilderDuBuild scipy "$SCIPY_BUILD_ARGS" "$SCIPY_ENV" "$SCIPY_CONFIG_ARGS"
   else
 # Building at this point as only one package, and that checked by bilderUnpack
-    cd $BUILD_DIR/scipy-${SCIPY_BLDRVERSION}
     cmd="python -m pip install --upgrade --target=$MIXED_PYTHON_SITEPKGSDIR -i https://pypi.binstar.org/carlkl/simple scipy"
     techo "$cmd"
     if $cmd; then
